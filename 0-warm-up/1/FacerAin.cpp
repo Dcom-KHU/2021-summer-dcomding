e32b932248903665637fc3ec8ccdc50fd9da69bd
FacerAin <ywsong.dev@kakao.com>
2021-06-26 04:54:03 +0000

    Edit 0-warm-up/1/

--- 0-warm-up/1/FacerAin.cpp
+++ 0-warm-up/1/FacerAin.cpp
@@ -6,7 +6,7 @@ int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for(int i = 1; i <=n; i++){
+	for(int i = 1; i <=3; i++){
 		cout << n << " * " << i << " = " << n*i << '\n';
 	}
 	return 0;
