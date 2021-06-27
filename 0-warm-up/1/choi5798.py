bddecc26d80b70cf805dfbf2035f993736eb7f04
choi5798 <vlflalektzja@naver.com>
2021-06-27 17:33:17 +0900

    blank test

--- 0-warm-up/1/choi5798.py
+++ 0-warm-up/1/choi5798.py
@@ -1,6 +1,4 @@
-n = int(input())
-total = 0
-for i in range(1, 4):
-    print('{} * {} = {}'.format(n, i, n*i))
-while True:
-    total += 1
+# n = int(input())
+# for i in range(1, 4):
+#     print('{} * {} = {}'.format(n, i, n*i))
+
