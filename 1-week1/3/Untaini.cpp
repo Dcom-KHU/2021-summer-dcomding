2062dbd37fc9c7f2c4673f5cfc009cc13ce06e3d
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-06-28 13:56:53 +0000

    upload 1-week1/3 solve code

---
+++ 1-week1/3/Untaini.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int n, numList[12], operatorCntList[4], minResult=1<<31-1, maxResult;
+
+void operateFunc(int cnt, int prevResult){
+	if(cnt==n){
+		minResult = min(minResult, prevResult);
+		maxResult = max(maxResult, prevResult);
+	}
+	else{
+		for(int opeCnt=0; opeCnt<4; ++opeCnt){
+			if(operatorCntList[opeCnt]){
+				--operatorCntList[opeCnt];
+				switch(opeCnt){
+					case 0: operateFunc(cnt+1, prevResult+numList[cnt]); break;
+					case 1: operateFunc(cnt+1, prevResult-numList[cnt]); break;
+					case 2: operateFunc(cnt+1, prevResult*numList[cnt]); break;
+					case 3: operateFunc(cnt+1, prevResult/numList[cnt]); break;
+				}
+				++operatorCntList[opeCnt];
+			}
+		}
+	}
+}
+
+int main() {
+	scanf("%d",&n);
+	for(int cnt=0; cnt<n; ++cnt)
+		scanf("%d",&numList[cnt]);
+	for(int cnt=0; cnt<4; ++cnt)
+		scanf("%d",&operatorCntList[cnt]);
+	
+	operateFunc(1,numList[0]);
+	printf("%d\n%d",maxResult,minResult);
+	return 0;
+}
