f30550a3a4ff0fb56aedbda5ef59e2395514dcda
Jinsu Park <dev.umijs@gmail.com>
2021-06-29 02:16:48 +0900

    진수: 숫자카드

--- 1-week1/1/umi0410.py
+++ 1-week1/1/umi0410.py
@@ -2,11 +2,15 @@
 length, numOfTurns = map(int, input().split())
 cards = sorted([int(card) for card in input().split()])
 
-for i in range(1, numOfTurns + 1):
-    if i % 2 == 1:
-        cards = cards[1:]
-    else:
-        cards = cards[:-1]
+# 굳이 이렇게 반복문을 돌릴 필요 없이
+# for i in range(1, numOfTurns + 1):
+#     if i % 2 == 1:
+#         cards = cards[1:]
+#     else:
+#         cards = cards[:-1]
+
+# 생각해보면 그냥 짤릴 앞 뒤는 정해져있으니
+cards = cards[(numOfTurns+1)//2:length - numOfTurns//2]
 
 print(sum(cards))
 
