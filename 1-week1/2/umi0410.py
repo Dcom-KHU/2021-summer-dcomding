c1868ef7b5e504021acd9fc76128ea7a54c1e047
Jinsu Park <dev.umijs@gmail.com>
2021-06-29 02:43:29 +0900

    진수: 하노이 설명 추가

--- 1-week1/2/umi0410.py
+++ 1-week1/2/umi0410.py
@@ -14,6 +14,11 @@ def get_rest_one(a, b):
 
 # 현재 타워가 src에서 dest로 가고 싶음
 # index로 계산하기 때문에 src=0인 경우 1번 타워에서 출발한다는 의미
+# 3개를 옮기는 경우 예시)
+# 1,2,3 고리 모두가 1번 타워에서 3번 타워로 가려면
+# 1)   1,2고리를 2번 타워로 옮김
+# 2)   3고리를 1번 타워에서 3번 타워로 옮김
+# 3)   다시 1,2 고리를 2번 타워에서 3번 타워로 옮김
 def move(current_top_value, src, dest):
     if current_top_value == 1:
         return dest + 1
