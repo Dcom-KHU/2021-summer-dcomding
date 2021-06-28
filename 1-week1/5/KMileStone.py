a26d0e7edc4f3e8fb60192e3d7fdca69e71f2879
KMileStone <tdj03204@naver.com>
2021-06-29 04:30:57 +0900

    submit 1-week1/5

--- 1-week1/5/KMileStone.py
+++ 1-week1/5/KMileStone.py
@@ -2,28 +2,23 @@ n = int(input())
 money = list(map(int, input().split()))
 
 
-# n = 3 : [1 2 3]           -> 1 2 3 1 2 3                  -> [1] [2] [3]
-# n = 4 : [1 2 3 4]         -> 1 2 3 4 1 2 3 4              -> [1 3] [2 4]
-# n = 5 : [1 2 3 4 5]       -> 1 2 3 4 5 1 2 3 4 5          -> [1 3] [2 4] [3 5] [4 1] [5 2]
-# n = 6 : [1 2 3 4 5 6]     -> 1 2 3 4 5 6 1 2 3 4 5 6      -> [1 3 5] [2 4 6]
-# n = 7 : [1 2 3 4 5 6 7]   -> 1 2 3 4 5 6 7 1 2 3 4 5 6 7  -> [1 3 5] [2 4 6] [3 5 7] [4 6 1] [5 7 2] [6 1 3] [7 2 4]
-# ...
+max_val = [0 for i in range(n)]
 
-# circulate money
-money_circ = money + money
+# recursive
+def steal(val, idx, head):
+    # head neighbor tail
+    if not(head == 0 and idx == n-1):
+        val += money[idx]
 
-val = 0
-max_val = 0
+        # update max
+        if val > max_val[head]:
+            max_val[head] = val
+
+            # steal more in non-neighbor
+            for i in range(2, n-idx):
+                steal(val, idx+i, head)
 
 for i in range(n):
-    val = 0
+    steal(0, i, i)
 
-    # you can steal maximum n//2 laptops in non-neighbor
-    for j in range(n//2):
-        val += money_circ[i + 2*j]
-
-    # update max
-    if val > max_val:
-        max_val = val
-
-print(max_val)
+print(max(max_val))
