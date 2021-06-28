3a2b4b39bde1a942da55b32391fe888ae9e0dc50
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-06-28 13:39:51 +0000

    upload 1-week1/2 solve code

---
+++ 1-week1/2/Untaini.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+long long result;
+int n;
+
+void hanoi(int diskNum, int startPillar, int endPillar, int remainPillar){
+	if(diskNum<1) return;
+	hanoi(diskNum-1, startPillar, remainPillar, endPillar);
+	result+=endPillar;
+	hanoi(diskNum-1, remainPillar, endPillar, startPillar);
+}
+
+int main() {
+	scanf("%d",&n);
+	hanoi(n,1,3,2);
+	printf("%lld",result);
+	return 0;
+}
