2e1214d5312272f3ccbad15af03c5533791a8da5
bluejoyq <bluejoyq@naver.com>
2021-06-28 15:22:27 +0000

    1-4 exception case 1

--- 1-week1/4/bluejoyq.py
+++ 1-week1/4/bluejoyq.py
@@ -2,33 +2,36 @@ import math
 
 N, K = map(int, input().split())
 values = list(map(int, input().split()))
-tree_length = pow(2,math.ceil(math.log(N,2))+1)-1
-tree_list = [0]*(tree_length)
-
-def init(node, start, end):
-    if start == end :
-        tree_list[node] = values[start]
-        return tree_list[node]
-    else :
-        tree_list[node] = max(init(node*2, start, (start+end)//2) , init(node*2+1, (start+end)//2+1, end))
-        return tree_list[node]
-
-def get_max(idx,left,right,start, end):
-    if left > end  or right < start:
-        return -1
-    if left <= start and end <= right:
-        return tree_list[idx]
+if N == 1:
+    print(values[0])
+else:
+    tree_length = pow(2,math.ceil(math.log(N,2))+1)-1
+    tree_list = [0]*(tree_length)
     
-    mid = (start + end) // 2
-    return max(get_max(idx * 2, left, right, start, mid), get_max(idx*2 + 1,left,right, mid+1, end))
-
-
-init(1,0,N-1)
-result = 12312312412
-for i in range(N - K):
-    left = i 
-    right = i + K 
-    tmp = get_max(1, left, right, 0, N)
-    result = min(result, tmp)
-print(result)
-#[2 ,4 ,5 ,3 ,2 ,1 ,4 ,2 ,5, 1]
+    def init(node, start, end):
+        if start == end :
+            tree_list[node] = values[start]
+            return tree_list[node]
+        else :
+            tree_list[node] = max(init(node*2, start, (start+end)//2) , init(node*2+1, (start+end)//2+1, end))
+            return tree_list[node]
+    
+    def get_max(idx,left,right,start, end):
+        if left > end  or right < start:
+            return -1
+        if left <= start and end <= right:
+            return tree_list[idx]
+        
+        mid = (start + end) // 2
+        return max(get_max(idx * 2, left, right, start, mid), get_max(idx*2 + 1,left,right, mid+1, end))
+    
+    
+    init(1,0,N-1)
+    result = 12312312412
+    for i in range(N - K):
+        left = i 
+        right = i + K 
+        tmp = get_max(1, left, right, 0, N)
+        result = min(result, tmp)
+    print(result)
+    #[2 ,4 ,5 ,3 ,2 ,1 ,4 ,2 ,5, 1]
