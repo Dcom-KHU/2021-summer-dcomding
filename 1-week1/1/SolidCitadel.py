4477527703ce2b1bd9adefa45c8e568bacf3511c
SolidCitadel <teddy3582@naver.com>
2021-06-28 16:38:44 +0900

    Update SolidCitadel.py

--- 1-week1/1/SolidCitadel.py
+++ 1-week1/1/SolidCitadel.py
@@ -1,10 +1,4 @@
 n, k = map(int, input().split(' '))
 arr = list(map(int, input().split(' ')))
 
-for i in range(1, k+1):
-    if i%2:
-        arr.remove(min(arr))
-    else:
-        arr.remove(max(arr))
-
-print(sum(arr))
+print(sum(sorted(arr)[k//2+k%2:n-(k//2)]))
