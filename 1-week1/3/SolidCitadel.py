d0356c04bbc7ebdf6a13f953ce8c3efe9bdb7c51
SolidCitadel <teddy3582@naver.com>
2021-06-29 22:17:45 +0900

    Update SolidCitadel.py

--- 1-week1/3/SolidCitadel.py
+++ 1-week1/3/SolidCitadel.py
@@ -6,9 +6,9 @@ arr = list(map(int, input().split(' ')))
 a, b, c, d = map(int, input().split(' '))
 
 maxi, mini = -10**9, 10**9
-for ops in permutations([[add]*a, [sub]*b, [mul]*c, [lambda x, y: x//y if x>0 else -(abs(x)//y)]*d]):
+for ops in permutations([add]*a + [sub]*b + [mul]*c + [lambda x, y: int(x/y)]*d):
     s = arr[0]
-    for i, op in zip(arr[1:], sum(ops, [])):
+    for i, op in zip(arr[1:], ops):
         s = op(s, i)
     maxi, mini = max(s, maxi), min(s, mini)
 
