5639ca41953ffcd701ba1790ca0e2b696fbb5ac3
choi5798 <vlflalektzja@naver.com>
2021-06-29 20:30:23 +0900

    1-1 2t

--- 1-week1/1/choi5798.py
+++ 1-week1/1/choi5798.py
@@ -1,8 +1,8 @@
 from collections import deque
 n, k = list(map(int, input().split()))
 cards = list(map(int, input().split()))
+cards = deque(sorted(cards))
 for i in range(1, k+1):
-    cards = deque(sorted(cards))
     if i%2 == 0:
         temp = cards.pop()
     else:
