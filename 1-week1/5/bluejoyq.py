d5ecd77fe492c39833280e05f4854a2d67461087
bluejoyq <bluejoyq@naver.com>
2021-06-29 11:31:32 +0000

    1-5 실수 수정

--- 1-week1/5/bluejoyq.py
+++ 1-week1/5/bluejoyq.py
@@ -4,18 +4,20 @@ def solve():
     
     # 원형이기 때문에 처음을 선택하는 경우와 선택하지 않는 경우를 다르게 세야함.
     
+    # 처음 것을 안 고르는 경우
     cache = [0] * (N + 3)
-    
     for i in range(1, N):
         cache[i+2] = max(cache[i+2], cache[i] + values[i])
-        cache[i+3] = cache[i+3], cache[i] + values[i]
-    
+        cache[i+3] = cache[i] + values[i]
     a = max(cache[N-1:])
+    
+    # 처음 것을 고르는 경우
     cache = [0] * (N + 3)
     for i in range(N - 1):
         cache[i+2] = max(cache[i+2], cache[i] + values[i])
         cache[i+3] = cache[i] + values[i]
     b = max(cache[N-1:])
+    
     print(max(a,b))
 
 solve()
