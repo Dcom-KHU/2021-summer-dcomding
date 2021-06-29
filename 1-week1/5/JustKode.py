7f17e85d9cbafb4cd92ca64593fcf62d52d82330
박민재 <sobu0715@gmail.com>
2021-06-30 04:49:04 +0900

    살아있다.

--- 1-week1/5/JustKode.py
+++ 1-week1/5/JustKode.py
@@ -1,13 +1,13 @@
 n = int(input())
 money = list(map(int, input().split()))
 
-dp = [0 for i in range(n)]
+dp = [0 for i in range(n)]  # 처음꺼 포함
 dp[0], dp[1] = money[0], money[0]
 
 for i in range(2, n - 1):
     dp[i] = max(dp[i - 1], dp[i - 2] + money[i])
 
-dp2 = [0 for i in range(n)]
+dp2 = [0 for i in range(n)]  # 끝꺼 포함
 dp2[0], dp2[1] = 0, money[1]
 
 for i in range(2, n):
