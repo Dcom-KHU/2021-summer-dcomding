694a61782828bd4adb68ec7fa6ebd2908271ba42
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-06-30 11:54:36 +0000

    upload 1-week1/1 another answer

--- 1-week1/1/Untaini.cpp
+++ 1-week1/1/Untaini.cpp
@@ -1,33 +1,31 @@
 #include <cstdio>
-#include <deque>
 #include <algorithm>
 using namespace std;
 
-
-long long sumResult;
-deque<int> numCards;
-int n, k, num;
+int cardCnt[100001], minPoint=100001, maxPoint, n, k, cardNum, sum;
 
 int main(){
 	scanf("%d%d",&n,&k);
 	for(int cnt=0;cnt<n;++cnt){
-		scanf("%d",&num);
-		numCards.push_back(num);
-		sumResult += num;
+		scanf("%d", &cardNum);
+		++cardCnt[cardNum];
+		minPoint = min(minPoint, cardNum);
+		maxPoint = max(maxPoint, cardNum);
+		sum+=cardNum;
 	}
 	
-	sort(numCards.begin(), numCards.end());
-	
-	for(int cnt=1;cnt<=k;++cnt)
+	for(int cnt=1;cnt<=k;++cnt){
 		if(cnt&1){
-			sumResult -= numCards.front();
-			numCards.pop_front();
+			while(!cardCnt[minPoint]) ++minPoint;
+			sum-=minPoint;
+			--cardCnt[minPoint];
 		}
 		else{
-			sumResult -= numCards.back();
-			numCards.pop_back();
+			while(!cardCnt[maxPoint]) --maxPoint;
+			sum-=maxPoint;
+			--cardCnt[maxPoint];
 		}
-	
-	printf("%lld", sumResult);
+	}
+	printf("%d",sum);
 	return 0;
-} 
+}
