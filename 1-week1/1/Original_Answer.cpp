6d2c3b9243bc2af7657ad5650a7622141b25a4f5
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-06-30 11:54:17 +0000

    upload 1-week1/1 original answer

---
+++ 1-week1/1/Original_Answer.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+
+long long sumResult;
+deque<int> numCards;
+int n, k, num;
+
+int main(){
+	scanf("%d%d",&n,&k);
+	for(int cnt=0;cnt<n;++cnt){
+		scanf("%d",&num);
+		numCards.push_back(num);
+		sumResult += num;
+	}
+	
+	sort(numCards.begin(), numCards.end());
+	
+	for(int cnt=1;cnt<=k;++cnt)
+		if(cnt&1){
+			sumResult -= numCards.front();
+			numCards.pop_front();
+		}
+		else{
+			sumResult -= numCards.back();
+			numCards.pop_back();
+		}
+	
+	printf("%lld", sumResult);
+	return 0;
+} 
