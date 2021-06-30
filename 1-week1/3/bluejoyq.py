c4cdc6ae845e1810204353ae52715b06e94806a5
bluejoyq <bluejoyq@naver.com>
2021-06-30 14:06:23 +0000

    1-3 원래 시간 보려고

--- 1-week1/3/bluejoyq.py
+++ 1-week1/3/bluejoyq.py
@@ -1,45 +1,41 @@
 import sys
 import math
+MAX = sys.maxsize
+N = int(input())
+values = list(map(int, input().split()))
+cals = list(map(int, input().split()))
+max_result = -MAX
+min_result = MAX
 
-def solve():
+cur = values[0]
+
+def find_max_min(now, cur):
     global max_result, min_result
-    MAX = sys.maxsize
-    max_result = -MAX
-    min_result = MAX
-    N = int(input())
-    values = list(map(int, input().split()))
-    cals = list(map(int, input().split()))
-    cur = values[0]
-
-    def find_max_min(N,values,cals, now, cur):
-        global max_result, min_result
-        if now == N:
-            max_result = max(max_result, cur)
-            min_result = min(min_result, cur)
-            return ;
-        now_val = values[now]
-        for cal_idx in range(4):
-            if not cals[cal_idx]:
-                continue
-            cals[cal_idx] -= 1
-            nxt = cur
-
-
-            if cal_idx == 0:
-                nxt = cur + now_val
-            elif cal_idx == 1:
-                nxt = cur - now_val
-            elif cal_idx == 2:
-                nxt = cur * now_val
+    if now == N:
+        max_result = max(max_result, cur)
+        min_result = min(min_result, cur)
+        return ;
+    now_val = values[now]
+    for cal_idx in range(4):
+        if not cals[cal_idx]:
+            continue
+        cals[cal_idx] -= 1
+        nxt = cur
+        
+        
+        if cal_idx == 0:
+            nxt = cur + now_val
+        elif cal_idx == 1:
+            nxt = cur - now_val
+        elif cal_idx == 2:
+            nxt = cur * now_val
+        else:
+            if cur < 0:
+                nxt = - math.floor(abs(cur) / now_val)
             else:
-                if cur < 0:
-                    nxt = - math.floor(abs(cur) / now_val)
-                else:
-                    nxt = math.floor(cur / now_val)
-            find_max_min(N,values,cals,now + 1 , nxt)
-            cals[cal_idx] += 1
-    find_max_min(N,values,cals, 1, cur)
-    print(max_result)
-    print(min_result)
-    
-solve()
+                nxt = math.floor(cur / now_val)
+        find_max_min(now + 1 , nxt)
+        cals[cal_idx] += 1
+find_max_min(1, cur)
+print(max_result)
+print(min_result)
