77c56fc3bfc5d86a38f9dcb9182116a01d5e7d29
Jaehooni <8362532@gmail.com>
2021-06-30 02:13:29 +0000

    Update 1-week1/2/Jaehooni.py

--- 1-week1/2/Jaehooni.py
+++ 1-week1/2/Jaehooni.py
@@ -1,61 +1,37 @@
-def empty(list):
-    if (len(list) == 0):
-        return True
-
-    else:
-        return False
-
-
-def chooseDirection(directionList):
-    direction = [1, 2, 3]
-    a, b = directionList[0], directionList[1]
-    direction.remove(a)
-    direction.remove(b)
-
-    return direction[0]
-
-
-def makeDirectionList(DirectionListList, number):
-
-    previousDirectionList = DirectionListList[number - 2]
-    currentDirectionList = DirectionListList[number - 1]
-
-    if (empty(previousDirectionList)):
-        makeDirectionList(DirectionListList, number - 1)
-
-    for direction in previousDirectionList:
-        start, end = direction[0], direction[1]
-        stop = chooseDirection(direction)
-        currentDirectionList.append([start, stop])
-        currentDirectionList.append([stop, end])
-
-
-def hanoiSum(directionListList, number):
-    if (number == 0):
-        return 0
-
-    elif (number == 1):
-        return 3
-
-    else:
-        sum = 0
-
-        if (empty(directionListList[number-1])):
-            makeDirectionList(directionListList, number)
-
-        for direction in directionListList[number-1]:
-            sum += direction[1]
-
-        return sum + hanoiSum(directionListList, number-1)
-
-
-directionLists = [[[1, 3]]]
-
-for i in range(1, 20):
+directionLists = []
+for i in range(1, 21):
     directionLists.append([])
 
+directionLists[0].append(3)
 
-inputNumber = int(input())
+
+def makeList(directionLists, number):
+    startPoint = 1
+
+    if (len(directionLists[number-2]) == 0):
+        makeList(directionLists, number-1)
+
+    for i in directionLists[number-2]:
+        middlePoint = 6
+        middlePoint -= startPoint
+        middlePoint -= i
+
+        directionLists[number-1].append(middlePoint)
+        directionLists[number-1].append(i)
+
+        startPoint = i
 
 
-print(hanoiSum(directionLists, inputNumber))
+def calc(directionLists, number):
+    sum = 0
+    for i in range(0, number):
+        for j in directionLists[i]:
+            sum += j
+
+    return sum
+
+
+a = int(input())
+
+makeList(directionLists, a)
+print(calc(directionLists, a))
