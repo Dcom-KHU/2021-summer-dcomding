eaeac69b9b3fae9c4c891d53ec4d7d32e1a7bab9
choi5798 <vlflalektzja@naver.com>
2021-06-30 18:20:46 +0900

    1-3

---
+++ 1-week1/3/choi5798.py
@@ -0,0 +1,35 @@
+from itertools import permutations
+def calculate(a, b, ch):
+    if ch == '+':
+        return a+b
+    elif ch == '-':
+        return a-b
+    elif ch == '*':
+        return a*b
+    elif ch == '/':
+        return int(a/b)
+
+n = int(input())
+nums = list(map(int, input().split()))
+symbol_counts = list(map(int, input().split()))
+
+SYM = ['+', '-', '*', '/']
+MAX = -9999
+MIN = 9999
+symbols = []
+for count, sym in zip(symbol_counts, SYM):
+    for j in range(count):
+        symbols.append(sym)
+        
+for syms in permutations(symbols, n-1):
+    temp = calculate(nums[0], nums[1], syms[0])
+    for i in range(2, n):
+        temp = calculate(temp, nums[i], syms[i-1])
+    if temp > MAX:
+        MAX = temp
+        # print('MAX:', syms)
+    if temp < MIN:
+        MIN = temp
+        # print('MIN:', syms)
+print(MAX)
+print(MIN)
