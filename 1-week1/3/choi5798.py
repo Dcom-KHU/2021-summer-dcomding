1d826bb8a81d29b224a33609534eaf111a44e967
choi5798 <vlflalektzja@naver.com>
2021-07-01 16:11:41 +0900

    확장

--- 1-week1/3/choi5798.py
+++ 1-week1/3/choi5798.py
@@ -1,5 +1,4 @@
 from itertools import permutations
-import numpy as np
 def calculate(a, b, ch):
     if ch == '+':
         return a+b
@@ -15,21 +14,21 @@ nums = list(map(int, input().split()))
 symbol_counts = list(map(int, input().split()))
 
 SYM = ['+', '-', '*', '/']
-MAX = -9999
-MIN = 9999
+MAX = -1000000001
+MIN = 1000000001
 symbols = []
 for count, sym in zip(symbol_counts, SYM):
     for j in range(count):
         symbols.append(sym)
         
 for syms in permutations(symbols, n-1):
-    temp = np.int64(calculate(nums[0], nums[1], syms[0]))
+    temp = calculate(nums[0], nums[1], syms[0])
     for i in range(2, n):
-        temp = np.int64(calculate(temp, nums[i], syms[i-1]))
+        temp = calculate(temp, nums[i], syms[i-1])
     if temp > MAX:
         MAX = temp
         # print('MAX:', syms)
-    elif temp < MIN:
+    if temp < MIN:
         MIN = temp
         # print('MIN:', syms)
 print(MAX)
