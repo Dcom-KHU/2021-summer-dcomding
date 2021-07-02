7b8f18c24ea36abf4a97309726f77d01a92454b9
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-07-02 12:06:05 +0000

    upload 1-week1/2 O(1) code

--- 1-week1/2/Untaini.cpp
+++ 1-week1/2/Untaini.cpp
@@ -1,17 +1,14 @@
 #include <cstdio>
 
-long long result;
+unsigned long long result=1, expTwo=1;
 int n;
 
-
+//1 <= n <= 62 계산 가능
+//식은.. 원반이 처음에 위치한 자리에 따리 거쳐가는 막대의 경로가 같기 때문에
+//그것을 이용한 O(1)의 일반향입니다.. f(n) = n + 2^(n+1) - 2
 int main() {
 	scanf("%d",&n);
-	for(int i=1;i<=n;++i)
-		if((n-i)&1)
-			result += (1<<n-i+1) + ((1<<n-i)%3!=1?3-(1<<n-i)%3:0);
-		else
-			result += (1<<(n-i+1)) + 1;
-		
-	printf("%lld",result);
+	result<<=n+1;
+	printf("%llu",n+result-2);
 	return 0;
 }
