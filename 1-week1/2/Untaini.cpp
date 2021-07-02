7e19c3a36b36a7b7513b09f2eb20182808400488
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-07-02 12:41:26 +0000

    delete unused variable

--- 1-week1/2/Untaini.cpp
+++ 1-week1/2/Untaini.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 
-unsigned long long result=1, expTwo=1;
+unsigned long long result=1;
 int n;
 
 //1 <= n <= 62 계산 가능
 //식은.. 원반이 처음에 위치한 자리에 따리 거쳐가는 막대의 경로가 같기 때문에
 //그것을 이용한 O(1)의 일반향입니다.. f(n) = n + 2^(n+1) - 2
+
 int main() {
 	scanf("%d",&n);
 	result<<=n+1;
