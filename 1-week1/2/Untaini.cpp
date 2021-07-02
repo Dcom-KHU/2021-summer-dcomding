7926a8434f04796a460569841169aa11b323b31a
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-07-02 11:39:31 +0000

    upload 1-week1/2 another code

--- 1-week1/2/Untaini.cpp
+++ 1-week1/2/Untaini.cpp
@@ -3,16 +3,15 @@
 long long result;
 int n;
 
-void hanoi(int diskNum, int startPillar, int endPillar, int remainPillar){
-	if(diskNum<1) return;
-	hanoi(diskNum-1, startPillar, remainPillar, endPillar);
-	result+=endPillar;
-	hanoi(diskNum-1, remainPillar, endPillar, startPillar);
-}
 
 int main() {
 	scanf("%d",&n);
-	hanoi(n,1,3,2);
+	for(int i=1;i<=n;++i)
+		if((n-i)&1)
+			result += (1<<n-i+1) + ((1<<n-i)%3!=1?3-(1<<n-i)%3:0);
+		else
+			result += (1<<(n-i+1)) + 1;
+		
 	printf("%lld",result);
 	return 0;
-} 
+}
