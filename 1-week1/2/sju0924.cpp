19607f1dae95209971f18f55dcaa3020b986065a
ZION <sju0924@khu.ac.kr>
2021-07-02 23:52:43 +0900

    Update sju0924.cpp

--- 1-week1/2/sju0924.cpp
+++ 1-week1/2/sju0924.cpp
@@ -2,12 +2,9 @@
 
 using namespace std;
 
-unsigned long long int H[16] = { 0, };
-
 int hanoi(int a,int b,int c) {
 	if (a == 1)return c;
-	if(!H[a]) H[a]= hanoi(a - 1,b,6-b-c) + hanoi(1,b,c) + hanoi(a - 1,6-b-c,c);
-	return H[a];
+	return hanoi(a - 1,b,6-b-c) + hanoi(1,b,c) + hanoi(a - 1,6-b-c,c);
 }
 
 int main() {
