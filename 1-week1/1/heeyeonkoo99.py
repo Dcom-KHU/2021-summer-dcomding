8fb051dbc48909c88c72ffe13de3c0d27d1b072b
Heeyeon Koo <rndus0819@naver.com>
2021-07-03 23:57:39 +0900

    .

--- 1-week1/1/heeyeonkoo99.py
+++ 1-week1/1/heeyeonkoo99.py
@@ -1,17 +1,2 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[ ]:
-
-
-#1주차 수업-1
-n,k=map(int,input().split())
-temp=list(map(int,input().split()))
-idx=0
-while idx!=k:
-    if idx%2==0:
-        temp.remove(min(temp))
-    else:
-        temp.remove(max(temp))
-    idx+=1
-print(sum(temp))
