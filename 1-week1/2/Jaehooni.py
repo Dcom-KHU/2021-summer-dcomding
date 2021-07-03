e41e1b78c57b83bf300bd52b2770bd3b209db198
Jaehooni <8362532@gmail.com>
2021-07-03 19:40:35 +0000

    text

--- 1-week1/2/Jaehooni.py
+++ 1-week1/2/Jaehooni.py
@@ -51,18 +51,23 @@ directionLists[0].append(3)
 def makeList(directionLists, number):
     startPoint = 1
 
-    if (len(directionLists[number-2]) == 0):
-        makeList(directionLists, number-1)
+    if (number == 1):
+        pass
 
-    for i in directionLists[number-2]:
-        middlePoint = 6
-        middlePoint -= startPoint
-        middlePoint -= i
+    else:
 
-        directionLists[number-1].append(middlePoint)
-        directionLists[number-1].append(i)
+        if (len(directionLists[number-2]) == 0):
+            makeList(directionLists, number-1)
 
-        startPoint = i
+        for i in directionLists[number-2]:
+            middlePoint = 6
+            middlePoint -= startPoint
+            middlePoint -= i
+
+            directionLists[number-1].append(middlePoint)
+            directionLists[number-1].append(i)
+
+            startPoint = i
 
 
 def calc(directionLists, number):
