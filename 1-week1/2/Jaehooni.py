a0e143048129c7f07c5cbc0845625d6699a8b694
Jaehooni <8362532@gmail.com>
2021-07-03 19:34:23 +0000

    test

--- 1-week1/2/Jaehooni.py
+++ 1-week1/2/Jaehooni.py
@@ -1,42 +1,80 @@
-directionLists = []
-sumList = []
+# directionLists = []
+# sumList = []
 
+# for i in range(1, 21):
+#     directionLists.append([])
+#     sumList.append(0)
+
+# directionLists[0].append(3)
+# sumList[0] = 3
+
+
+# def makeList(directionLists, number):
+#     startPoint = 1
+
+#     if (number == 1):
+#         pass
+
+#     else:
+#         if (len(directionLists[number-2]) == 0):
+#             makeList(directionLists, number-1)
+
+#         for i in directionLists[number-2]:
+#             middlePoint = 6 - (startPoint + i)
+
+#             directionLists[number-1].append(middlePoint)
+#             directionLists[number-1].append(i)
+#             sumList[number-1] += (middlePoint + i)
+
+#             startPoint = i
+
+
+# def calc(number):
+#     if (number == 1):
+#         return 3
+
+#     return sumList[number - 1] + calc(number-1)
+
+
+# a = int(input())
+
+# makeList(directionLists, a)
+# print(calc(a))
+
+directionLists = []
 for i in range(1, 21):
     directionLists.append([])
-    sumList.append(0)
 
 directionLists[0].append(3)
-sumList[0] = 3
 
 
 def makeList(directionLists, number):
     startPoint = 1
 
-    if (number == 1):
-        pass
+    if (len(directionLists[number-2]) == 0):
+        makeList(directionLists, number-1)
 
-    else:
-        if (len(directionLists[number-2]) == 0):
-            makeList(directionLists, number-1)
+    for i in directionLists[number-2]:
+        middlePoint = 6
+        middlePoint -= startPoint
+        middlePoint -= i
 
-        for i in directionLists[number-2]:
-            middlePoint = 6 - (startPoint + i)
+        directionLists[number-1].append(middlePoint)
+        directionLists[number-1].append(i)
 
-            directionLists[number-1].append(middlePoint)
-            directionLists[number-1].append(i)
-            sumList[number-1] += (middlePoint + i)
-
-            startPoint = i
+        startPoint = i
 
 
-def calc(number):
-    if (number == 1):
-        return 3
+def calc(directionLists, number):
+    sum = 0
+    for i in range(0, number):
+        for j in directionLists[i]:
+            sum += j
 
-    return sumList[number - 1] + calc(number-1)
+    return sum
 
 
 a = int(input())
 
 makeList(directionLists, a)
-print(calc(a))
+print(calc(directionLists, a))
