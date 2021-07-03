bd6a37cc214828c2ca51d4d4d3467013ee41911b
Heeyeon Koo <rndus0819@naver.com>
2021-07-04 00:22:48 +0900

    ..

--- 1-week1/1/heeyeonkoo99.py
+++ 1-week1/1/heeyeonkoo99.py
@@ -1,7 +1,9 @@
 #1주차 수업-1
 import sys
+from collections import deque
+
 n,k=map(int,sys.stdin.readline().split())
-temp=list(map(int,sys.stdin.readline().split()))
+temp=deque(map(int,sys.stdin.readline().split()))
 temp.sort()
 for idx in range(1,k+1):
     if idx%2==1:
