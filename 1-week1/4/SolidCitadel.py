d852e6f14c7e2916ed79775e67b96f36e5b6f686
SolidCitadel <teddy3582@naver.com>
2021-07-04 19:03:42 +0900

    Update SolidCitadel.py

--- 1-week1/4/SolidCitadel.py
+++ 1-week1/4/SolidCitadel.py
@@ -1,7 +1,7 @@
 n, k = map(int, input().split())
 stones = list(map(int, input().split()))
 
-left, right = 1, n
+left, right = 1, 2*10**8
 while left <= right:
     mid = (left+right)//2
     count = k
