c7c2d424dd5c39d5e8893b1c97b86303eb4f526d
hyeweon <berry_na@khu.ac.kr>
2021-07-04 23:22:13 +0900

    Create hyeweon.cpp
    -

---
+++ 1-week1/5/hyeweon.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum = 0;
+	cin >> n;
+	int *money = new int[n+1];
+	for (int i = 0; i < n; i++)
+		cin >> money[i];
+	money[n] = money[0];
+
+	for (int i = 1; i < n;) {
+		if (money[i] > money[i - 1] + money[i + 1]) {
+			sum += money[i];
+			i += 3;
+		}
+		else {
+			sum += (money[i - 1] + money[i + 1]);
+			i += 4;
+		}
+	}
+
+	cout << sum << endl;
+}
