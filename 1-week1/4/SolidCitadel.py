b0f0d8363b15e540b7d6d9eb30f8ba0482681eb7
SolidCitadel <teddy3582@naver.com>
2021-07-04 19:02:54 +0900

    Create SolidCitadel.py

---
+++ 1-week1/4/SolidCitadel.py
@@ -0,0 +1,20 @@
+n, k = map(int, input().split())
+stones = list(map(int, input().split()))
+
+left, right = 1, n
+while left <= right:
+    mid = (left+right)//2
+    count = k
+    for s in stones:
+        if s <= mid:
+            count -= 1
+        else:
+            count = k
+        if not count:
+            break
+    if count:
+        left = mid + 1
+    else:
+        right = mid - 1
+    
+print(left)
