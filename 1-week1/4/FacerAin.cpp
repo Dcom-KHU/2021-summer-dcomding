5159997fc40b3c457d8803cd1d0d2c99eb6e904d
FacerAin <ywsong.dev@kakao.com>
2021-07-04 06:37:37 +0000

    Add 1-week1/4/

--- 1-week1/4/FacerAin.cpp
+++ 1-week1/4/FacerAin.cpp
@@ -1,21 +1,88 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#define INF 999999999;
+using namespace std;
+/*
+슬라이딩 윈도우 + 힙 구조
+출처: https://sina-sina.tistory.com/39
+*/
+int n,k;
+vector<int> stones;
+int main(){
+		cin >> n >> k;
+	stones.resize(n);
+	for(int i = 0; i < n; i++){
+		cin >> stones[i];
+	}
+	
+	multimap<int, int> m;
+    multimap<int, int>::iterator iter;
+	
+	for(int i = 0; i < k; i++){
+		m.insert({stones[i], -i});
+	}
+	
+	int min = INF;
+	iter = m.end();
+	int max = (*--iter).first;
+	if(min > max){
+		min = max;
+	}
+	
+	for(int i = k; i < stones.size(); i++){
+		iter = m.find(stones[i-k]);
+		m.erase(iter);
+		m.insert({stones[i], -i});
+		
+		iter = m.end();
+		max = (*--iter).first;
+		if(min > max){
+			min = max;
+		}
+		
+	}
+	cout << min;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <iostream>
+#include <vector>
 #include <algorithm>
 #include <queue>
 using namespace std;
 priority_queue<int,vector<int>,greater<int> > pq;
 
-/*
+
 stone이 2억, n이 20만이기 때문에 
 단순 시뮬레이션으로는 풀 수 없다. 다른 방법을 생각해보자.
 
 더 좋은 방법이 있을까?
 
-ans를 어떻게 업데이트 할 것인가?
-성공했다면 ans 갱신
-아니라면 출력
 
-*/
+
 int n, k;
 vector<int> stones;
 int ans = 1;
@@ -58,4 +125,4 @@ int main(){
 	return 0;
 	
 }
-
+*/
