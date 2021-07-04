8a36e24935b56b999c392261974eaf1ea5a1bc31
bluejoyq <bluejoyq@naver.com>
2021-07-04 15:20:59 +0000

    solve 2-2

---
+++ 2-week2/2/bluejoyq.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution():
+    s = deque(input())
+    n = len(s)
+    answer = 0
+    for i in range(n):
+        check = []
+        couples = {"(":")", "[": "]","{":"}" }
+        is_valid = True
+        for j in range(n):
+            cur = s[j]
+            if cur in ("(","[","{"):
+                check.append(cur)
+            else:
+                try:
+                    if couples[check[-1]] == cur:
+                        check.pop()
+                    else:
+                        raise()
+                except:
+                    is_valid = False
+                    break
+        if is_valid and not len(check):
+            answer += 1
+        s.append(s.popleft())
+    
+    
+    return answer
+print(solution())
