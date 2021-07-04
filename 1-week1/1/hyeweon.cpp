1b827d9cbd9ec49ab9521ac7558adb01aaf9770e
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-04 19:33:46 +0900

    1-week1

---
+++ 1-week1/1/hyeweon.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int sum = 0;
+	int min_n = (k + 1) / 2;
+	int max_n = k / 2;
+	int* card = new int[n];
+	int* min_array = new int[min_n];
+	int* max_array = new int[max_n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> card[i];
+		sum += card[i];
+	}
+
+	int min_index = 0; //최소값 중 최대
+	int max_index = 0; //최대값 중 최소
+
+	for (int i = 0; i < min_n; i++) {
+		min_array[i] = card[i];
+		if (min_array[i] > min_array[min_index])
+			min_index = i;
+	}
+
+	for (int i = 0; i < max_n; i++) {
+		max_array[i] = card[i];
+		if (max_array[i] < max_array[max_index])
+			max_index = i;
+	}
+
+	for (int i = min_n; i < n; i++) {
+		if (min_array[min_index] > card[i]) {
+			min_array[min_index] = card[i];
+			for (int j = 0; j < min_n; j++) {
+				if (min_array[j] > min_array[min_index])
+					min_index = j;
+			}
+		}
+	}
+
+	for (int i = max_n; i < n; i++) {
+		if (max_array[max_index] < card[i]) {
+			max_array[max_index] = card[i];
+			for (int j = 0; j < max_n; j++) {
+				if (max_array[j] < max_array[max_index])
+					max_index = j;
+			}
+		}
+	}
+
+	int min_sum = 0;
+	int max_sum = 0;
+
+	for (int i = 0; i < min_n; i++) {
+		min_sum += min_array[i];
+	}
+
+	for (int i = 0; i < max_n; i++) {
+		max_sum += max_array[i];
+	}
+
+	cout << sum - min_sum - max_sum << endl;
+}
