d621811543c46b65bf2446e50f5e9ea2cfa192f7
Jaehooni <8362532@gmail.com>
2021-07-04 14:34:16 +0000

    text

--- 1-week1/4/Jaehooni.py
+++ 1-week1/4/Jaehooni.py
@@ -1,7 +1,7 @@
 import heapq
 
 length, maxDistance = map(int, input().split())
-stonesNumber = list(map(int,input().split()))
+stonesNumber = list(map(int, input().split()))
 min_in_max_nums = float('inf')
 
 for i in range(0, len(stonesNumber)-maxDistance+1):
@@ -9,7 +9,7 @@ for i in range(0, len(stonesNumber)-maxDistance+1):
     for j in range(i, i+maxDistance):
         value = stonesNumber[j]
         heapq.heappush(max_heap, (-value, value))
-    
+
     min_in_max_nums = min(min_in_max_nums, heapq.heappop(max_heap)[1])
-            
+
 print(min_in_max_nums)
