1b1de76fa12ef9c335e3d74c18b89e97d903c5eb
hyeweon <berry_na@khu.ac.kr>
2021-07-04 21:17:21 +0900

    Create hyeweon.cpp
    2-week1

---
+++ 1-week1/2/hyeweon.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum = 0;
+
+void hanoi(int n, int from, int to, int temp) {
+	if (n == 1) 
+		sum += to;
+	else {
+		hanoi(n - 1, from, temp, to);
+		sum += to;
+		hanoi(n - 1, temp, to, from);
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	hanoi(n, 1, 3, 2);
+	cout << sum << endl;
+}
