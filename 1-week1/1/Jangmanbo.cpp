6d7bd6f4ec491b153c3f4ffe48d41d54cc2afc9b
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-04 18:57:29 +0900

    1-1

--- 1-week1/1/Jangmanbo.cpp
+++ 1-week1/1/Jangmanbo.cpp
@@ -16,13 +16,14 @@ void insertionSort(int arr[], int size) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
     int n, k, start, end, sum = 0;
-	cin >> n >> k;
-	int* num = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		cin >> num[i];
-	}
+    cin >> n >> k;
+    int* num = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num[i];
+    }
     insertionSort(num, n);
     start = (k - 1) / 2 + 1;
     end = start + (n - k);
