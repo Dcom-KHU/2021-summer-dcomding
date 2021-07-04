dd9370e42c7c86db6a8d974e93acafa5f1b7aecf
ChangMok Yoon <riverboy94@khu.ac.kr>
2021-07-04 17:20:55 +0900

    W1-Q5

--- 1-week1/5/all-eviate.py
+++ 1-week1/5/all-eviate.py
@@ -1,29 +1,17 @@
 n = int(input(""))
 money = list(map(int, input("").split()))
-t1 = 0
-t2 = 0
-mt1 = money[:]
-mt2 = money[:]
-mt2[mt2.index(max(mt2))] = 0
-while max(mt1) > 0:
-    m1 = max(mt1)
-    t1 += m1
-    ind = mt1.index(m1)
-    for i in range(ind-1, ind+2):
-        if i >= 0 and i < n:
-            mt1[i] = 0
-        else:
-            mt1[(i+n)%n] = 0
-    m2 = max(mt2)
-    t2 += m2
-    ind = mt2.index(m2)
-    for i in range(ind-1, ind+2):
-        if i >= 0 and i < n:
-            mt2[i] = 0
-        else:
-            mt2[(i+n)%n] = 0
-#while max(mt2) > 0:
-if t1 >= t2:
-    print(t1)
-else:
-    print(t2)
+steal1 = [0 for i in range(n)]
+steal2 = [0 for i in range(n)]
+
+#first included
+steal1[0] = steal1[1] = money[0]
+for i in range(2, n):
+    steal1[i] = max(steal1[i-1], steal1[i-2] + money[i])
+
+#first excluded
+steal2[0] = 0
+steal2[1] = money[1]
+for i in range(2, n):
+    steal2[i] = max(steal2[i-1], steal2[i-2] + money[i])
+
+print(max(steal1[-2], steal2[-1]))
