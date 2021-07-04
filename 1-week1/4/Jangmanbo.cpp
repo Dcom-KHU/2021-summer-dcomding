2d9acaecf5c59aef9dcef0f2c26937481a48251e
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-04 23:04:33 +0900

    1-4

---
+++ 1-week1/4/Jangmanbo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k, people = -1, skip = 0;
+    bool next = true;
+    cin >> n >> k;
+    int* stones = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> stones[i];
+    }
+    k--;
+    while (next) {
+        people++;
+        for (int i = 0; i < n; i++)
+        {
+            if (stones[i] > 0) {
+                stones[i]--;
+                skip = 0;
+            }
+            else if (skip < k) {
+                skip++;
+            }
+            else {
+                next = false;
+                skip = 0;
+                break;
+            }
+        }
+    }
+    cout << people;
+    return 0;
+}
