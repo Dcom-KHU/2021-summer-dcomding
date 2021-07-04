3051eae146783511feb587d8b7307774d960039f
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-07-04 17:54:15 +0000

    edit 2-week2/2 code.....

--- 2-week2/2/Untaini.cpp
+++ 2-week2/2/Untaini.cpp
@@ -1,31 +1,31 @@
 #include <cstdio>
 #include <cstring>
 
-char str[1001];
+char str[1010];
 int len, result;
 
 bool checkCorrectBracket(int startPos){
-	int bracketCnt[3][2] = {}, pos=startPos;
+	int bracketCnt[3] = {}, pos=startPos;
 	do{
 		char curLetter = str[pos%len];
 		int selBracket = curLetter/40-1;
 		switch(curLetter){
 			case '(': case '[': case '{': 
-				++bracketCnt[selBracket][0];
+				++bracketCnt[selBracket];
 				break;
 			case ')': case ']': case '}':
-				++bracketCnt[selBracket][1];
-				if(bracketCnt[selBracket][0] < bracketCnt[selBracket][1])
+				--bracketCnt[selBracket];
+				if(bracketCnt[selBracket]<0)
 					return false;
 				break;
 		}
 		pos=(pos+1)%len;
 	}while(pos!=startPos);
 	
-	bool result = true;
+	bool res = true;
 	for(int cnt=0; cnt<3; ++cnt)
-		result &= (bracketCnt[cnt][0] == bracketCnt[cnt][1]);
-	return result;
+		res &= !bracketCnt[cnt];
+	return res;
 }
 
 int main() {
@@ -36,6 +36,6 @@ int main() {
 		if(checkCorrectBracket(cnt))
 			++result;
 		
-	printf("%d",len==1?0:result);
+	printf("%d",len%2==1?0:result);
 	return 0;
 }
