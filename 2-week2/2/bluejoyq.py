c4abf4127e7979f8fbc9885a9baf93b427bdd40f
bluejoyq <bluejoyq@naver.com>
2021-07-05 06:10:58 +0000

    2-2 update for better code

--- 2-week2/2/bluejoyq.py
+++ 2-week2/2/bluejoyq.py
@@ -8,23 +8,23 @@ def solution():
         check = []
         couples = {"(":")", "[": "]","{":"}" }
         is_valid = True
+        
+        # 올바른 괄호인지 체크한다.
         for j in range(n):
             cur = s[j]
             if cur in ("(","[","{"):
                 check.append(cur)
-            else:
-                try:
-                    if couples[check[-1]] == cur:
-                        check.pop()
-                    else:
-                        raise()
-                except:
-                    is_valid = False
-                    break
+                continue
+            try:
+                # 반대편 괄호가 들어올 경우 항상 stack의 최상단과 짝이 맞야아 한다.
+                if couples[check.pop()] != cur:
+                    raise()
+            except:
+                is_valid = False
+                break
         if is_valid and not len(check):
             answer += 1
         s.append(s.popleft())
-    
-    
+        
     return answer
 print(solution())
