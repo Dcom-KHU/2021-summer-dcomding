658d30dea2e105075518a195cc9e342c0bc8b59f
FacerAin <ywsong.dev@kakao.com>
2021-07-05 14:20:00 +0000

    Edit 2-week2/3/

--- 2-week2/3/FacerAin.cpp
+++ 2-week2/3/FacerAin.cpp
@@ -1,77 +1,50 @@
 #include <iostream>
-#include <string>
-#include <map>
 #include <vector>
 #include <algorithm>
 using namespace std;
-vector<vector<string>> v;
-vector<string> q;
-map<string, int> m;
-int check[5005];
-/*
-자료형을 어떻게 구성할까?
-
-*/
-bool comp(string a, string b){
-	if(a.length() != b.length()){
-		return a.length() > b.length();
-	}else{
-		return a > b;
-	}
-}
-
-void BFS(){
-	
-	q.push_back("DCOM");
-	while(!q.empty()){
-		string club = q.back();
-		q.pop_back();
-		int c_idx = m[club];
-		check[c_idx] = 1;
-		cout << club << " ";
-		
-		for(int i = 0; i < v[c_idx].size(); i++){
-			string next_club = v[c_idx][i];
-			v[c_idx][i] = "";
-			if(next_club != ""){
-				q.push_back(next_club);
-			}
-			
-
-			
-		}
-		sort(q.begin(), q.end(), comp);
-	}
-	
-}
+vector<vector<string>> ticket_v;
+vector<string> answer;
+int check[100005];
 int n;
+bool comp(const vector<string>& v1, const vector<string>& v2){
+	if(v1[1].length() != v2[1].length()){
+		return v1[1].length() < v2[1].length();
+	}
+	return v1[1] < v2[1];
+}
+bool DFS(string club, int visit_num){
+	answer.push_back(club);
+	if(visit_num == n){
+		return true;
+	}
+	for(int i = 0; i < ticket_v.size(); i++){
+		if(ticket_v[i][0] == club && !check[i]){
+			check[i] = 1;
+			if(DFS(ticket_v[i][1], visit_num+1)){
+				return true;
+			}
+			check[i] = 0;
+		}
+	}
+	answer.pop_back();
+}
+
 int main(){
 	cin >> n;
-	int idx = 0;
-	
 	for(int i = 0; i < n; i++){
 		string s1, s2;
 		cin >> s1 >> s2;
-		if(m.find(s1) != m.end()){//시작 정점이 있을때
-			v[m[s1]].push_back(s2);
-		}else{//시작 정점이 없을때
-			m.insert({s1,idx});
-			vector<string> temp_v;
-			v.push_back(temp_v);
-			v[idx].push_back(s2);
-			idx++;
-		}
-		//끝 정점이 없을때
-		if(m.find(s2) == m.end()){
-			m.insert({s2,idx});
-			vector<string> temp_v;
-			v.push_back(temp_v);
-			idx++;
-		}
+		vector<string> temp_v(2);
+		temp_v[0] = s1;
+		temp_v[1] = s2;
+		ticket_v.push_back(temp_v);
 	}
+	sort(ticket_v.begin(),ticket_v.end(),comp);
 	
-	BFS();
-	
-	
+	DFS("DCOM",0);
+	for(int i = 0; i < answer.size(); i++){
+		cout << answer[i] << " ";
+	}
 	return 0;
+	
 }
