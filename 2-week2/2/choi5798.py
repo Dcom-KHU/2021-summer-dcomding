f90d560e19c291940429624200ed8f9fa76a7176
choi5798 <vlflalektzja@naver.com>
2021-07-05 16:57:43 +0900

    2-2

---
+++ 2-week2/2/choi5798.py
@@ -0,0 +1,31 @@
+s = input()
+def solution(s):
+    def rotate(s, x):
+        return s[x:]+s[:x]
+    def check(s):
+        if len(s)%2 != 0:
+            return False
+        stack = []
+        last_open = ''
+        for ch in s:
+            if ch in '({[':
+                stack.append(ch)
+            elif stack:
+                top = stack.pop()
+                if (ch == ')' and top == '(') or (ch == '}' and top == '{') or (ch == ']' and top == '['):
+                    continue
+                else:
+                    return False
+            else:
+                return False
+        if stack:
+            return False
+        return True
+
+    answer = 0
+    for x in range(len(s)):
+        rotated_s = rotate(s, x)
+        if check(rotated_s):
+            answer+=1
+    return answer
+print(s)
