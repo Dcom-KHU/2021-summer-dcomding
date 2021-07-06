ad89a5797567938a2f62763e3f69ebed5dd5a0df
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-07-06 13:09:54 +0000

    edit 2-week2/5 code

--- 2-week2/5/Untaini.cpp
+++ 2-week2/5/Untaini.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int n, arrow, x, y, dx[] = {1,0,-1}, dy[]={1,0,-1}, result;
 set<pii> visitedLocSet;
-set<piiii> drewEdgeSet;
+set<piiii> drewEdgeSet, diagonalEdgeSet;
 vector<piiii> edgeList;
 pii prevVertex = make_pair(0,0), nextVertex;
 piiii edge;
@@ -22,13 +22,13 @@ int main() {
 		arrow = (arrow+2)%8;
 		y += dy[!(arrow%4) + 2*(arrow>4)];
 		
-		
 		nextVertex = make_pair(x, y);
 		edge = make_pair(min(prevVertex, nextVertex), max(prevVertex, nextVertex));
 		if(!drewEdgeSet.count(edge)) {
 			drewEdgeSet.insert(edge);
 			edgeList.push_back(edge);
 		}
+		
 		prevVertex = nextVertex;
 		
 	}
@@ -41,6 +41,13 @@ int main() {
 		
 		if(!visitedPrevVertex) visitedLocSet.insert(prevVertex);
 		if(!visitedNextVertex) visitedLocSet.insert(nextVertex);
+		
+		int pvx = prevVertex.first, pvy = prevVertex.second, nvx = nextVertex.first, nvy = nextVertex.second;
+		if(pvx != nvx && pvy != nvy){
+			pii vertex1 = make_pair(nvx, pvy), vertex2 = make_pair(pvx, nvy);
+			if(diagonalEdgeSet.count(make_pair(min(vertex1, vertex2), max(vertex1, vertex2)))) ++result;
+			diagonalEdgeSet.insert(edgeList[cnt]);
+		}
 	}
 	
 	printf("%d",result);
