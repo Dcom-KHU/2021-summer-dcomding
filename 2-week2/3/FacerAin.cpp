8f2b845c637461eee8b8df8e442a2bef22566dc9
FacerAin <ywsong.dev@kakao.com>
2021-07-06 13:44:40 +0000

    Update 2-3

--- 2-week2/3/FacerAin.cpp
+++ 2-week2/3/FacerAin.cpp
@@ -60,7 +60,6 @@ int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int idx = 0;
 	cin >> n;
-	ticket_v.resize(n);
 	answer.reserve(n);
 	for(int i = 0; i < n; i++){
 		string s1, s2;
