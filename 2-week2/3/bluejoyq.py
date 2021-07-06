5e02a7bb01bae96628b651b56c5a45a909b59ce7
bluejoyq <bluejoyq@naver.com>
2021-07-06 09:49:52 +0000

    2-3 solve

--- 2-week2/3/bluejoyq.py
+++ 2-week2/3/bluejoyq.py
@@ -4,18 +4,22 @@ input = sys.stdin.readline
 def solution():
     n = int(input())
     can_go = {}
+    check = {}
     kinds = set()
     
     for i in range(n):
         start, end = input().split()   
         try:
             can_go[start].append(end)
+            check[start][end] = 1
         except:
             can_go[start] = [end]
+            check[start] = {end: 1}
             
     def rank(tmp):
         '''길이와 사전순을 기준으로 정렬'''
         return (len(tmp), tmp)
+    
     for k in can_go:
         # fix
         can_go[k] = deque(sorted(can_go[k],key = rank))
@@ -23,13 +27,16 @@ def solution():
     cur = 'DCOM'
     while True:
         print(cur, end =' ')
-
         cur_tickets = can_go[cur]
-        for nxt in cur_tickets:
-            cur_tickets.popleft()
-            break
-        
+        nxt = cur_tickets.popleft()
+        try:
+            if cur_tickets and not check[nxt][end]:
+                pass
+        except:
+            cur_tickets.append(nxt)
+            nxt = cur_tickets.popleft()
         cur = nxt
+        
         try:
             if not can_go[cur]:
                 raise()
@@ -37,4 +44,5 @@ def solution():
             print(cur, end =' ')
             break
 solution()
-        
+
+     
