493f0965a39e1ed4262122501bffdcc788a6ac85
bluejoyq <bluejoyq@naver.com>
2021-07-06 14:14:36 +0000

    2-1 패러티 비트 생성을 이상하게 하는 문제 해결

--- 2-week2/1/bluejoyq.py
+++ 2-week2/1/bluejoyq.py
@@ -1,23 +1,21 @@
 def solve():
     bits = list(map(int,input().split()))
     paritys = {
-        1: [3, 5, 7, 9, 11, 13, 15], 
-        2: [3, 6, 7, 10, 11, 14, 15], 
-        4: [5, 6, 7, 12, 13, 14, 15],
-        8: [9, 10, 11, 12, 13, 14, 15]
+        1: [3, 5, 7, 9, 11, 13, 15,1],
+        2: [3, 6, 7, 10, 11, 14, 15,2], 
+        4: [5, 6, 7, 12, 13, 14, 15,4],
+        8: [9, 10, 11, 12, 13, 14, 15,8]
     }
     datas = [3,5,6,7,9,10,11,12,13,14,15]
-    result = ""
+    error_idx = ""
     bits.insert(0,0)
     for p in paritys.values():
-        tmp = p[1]
+        tmp = bits[p[0]]
         for idx in p[1:]:
             tmp ^= bits[idx]
-        
-        tmp ^= tmp
-        result += str(tmp)
-
-    target_idx = int(result[::-1],2)
+        error_idx += str(tmp)
+    target_idx =int(error_idx[::-1],2)
+    
     bits[target_idx] = int(not bits[target_idx])
     result = ""
     for data in datas[::-1]:
