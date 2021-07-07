844fbef2a57505068e99ff72ca0cc8b9a017bafd
KMileStone <tdj03204@naver.com>
2021-07-07 09:17:17 +0900

    submit 2-week2/4

--- 2-week2/4/KMileStone.py
+++ 2-week2/4/KMileStone.py
@@ -5,44 +5,35 @@ for i in range(n):
     items.append(item)
 
 
-# num of item class
-n_item = len(set(items))
-
 min = 0
 max = n-1
+count = {i:0 for i in set(items)}
 left = 0
-right = n_item-1
-prev = 0
-n_sliced = len(set(items[left:right+1]))
+right = len(count.keys())-1
+for i in items[left:right+1]:
+    count[i] += 1
 
 while right < n:
-
     # if included all item
-    if n_sliced == n_item:
+    if 0 not in count.values():
         # update min max
         if right - left < max - min:
             min = left
             max = right
-            if max - min + 1 == n_item:
+            if max - min + 1 == len(count.keys()):
                 break
 
         # take less item
-        prev = left
+        count[items[left]] -= 1
         left += 1
-        while left <= right and items[left] == items[left+1]:
-            left += 1
-        if items[prev] not in items[left:right+1]:
-            n_sliced -= 1
 
     # if not included all item
     else:
         # take more item
-        prev = right
-        while right < n-1 and items[right] == items[right+1]:
-            right += 1
         right += 1
-        if right < n and items[right] not in items[left:prev+1]:
-            n_sliced += 1
+        if right < n:
+            count[items[right]] += 1
+
 
 # num of stand starts with 1
 min += 1
