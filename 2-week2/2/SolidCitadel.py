999797d43af51c7b688feb9f887371a661323a07
SolidCitadel <teddy3582@naver.com>
2021-07-07 14:50:57 +0900

    Update SolidCitadel.py

--- 2-week2/2/SolidCitadel.py
+++ 2-week2/2/SolidCitadel.py
@@ -1,24 +1,17 @@
-def findClose(s, key):
-    count = 0
-    for i in range(len(s)):
-        if s[i] == key[0]:
-            count += 1
-        elif s[i] == key[1]:
-            count -= 1
-            if count == 0:
-                return i
-    return -1
-        
 def isRight(s):
     if len(s):
         for key in ['[]', '()', '{}']:
             if s[0] == key[0]:
-                close = findClose(s, key)
-                if close != -1:
-                    if len(s[1:close]):
-                        return isRight(s[1:close]) and isRight(s[close+1:])
-                    else:
-                        return True
+                count = 0
+                for i in range(len(s)):
+                    if s[i] == key[0]:
+                        count += 1
+                    elif s[i] == key[1]:
+                        count -= 1
+                        if count == 0:
+                            return isRight(s[1:i]) and isRight(s[i+1:])
+    else:
+        return True
     return False
 
 s = input()
