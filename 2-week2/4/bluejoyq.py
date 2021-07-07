96d94699dc3746445faabeae2a9ba4e35c9776b7
bluejoyq <bluejoyq@naver.com>
2021-07-07 05:22:32 +0000

    2-4 check 방식을 변경

--- 2-week2/4/bluejoyq.py
+++ 2-week2/4/bluejoyq.py
@@ -1,34 +1,35 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 def solution():
     n = int(input())
     values = [0] * n
-    kinds = set()
+    count_dict = {}
     for i in range(n):
         data = input().rstrip()
         values[i] = data
-        kinds.add(data)
-    goal = len(kinds)
-    count_dict = {kind:0 for kind in kinds}
+        count_dict[data] = 0
+     
+    goal= len(count_dict)
     
-    cur = deque([])
-    result = 123124121
-    real_result = (0,0)
-    for i in range(n):
-        cur.append(values[i])
-        count_dict[values[i]] += 1
+    
+    def check():
+        for i in count_dict.values():
+            if not i:
+                return False
+        return True
         
-        cur_goal = sum(map(bool,count_dict.values()))
-        while goal == cur_goal:
-            len_cur = len(cur)
-            if result >len_cur :
-                result = len_cur
-                real_result = (i - len_cur + 1, i)
-            last= cur.popleft()
-            count_dict[last] -= 1
-            cur_goal = sum(map(bool,count_dict.values()))          
-    a,b = real_result
-    print(a+ 1)
-    print(b+1)
+    left = 0
+    result = 0
+    for right in range(n): 
+        count_dict[values[right]] += 1
+        if result:
+            count_dict[values[left]] -= 1
+            left += 1
+        while check():
+            result = [left + 1, right + 1]
+            count_dict[values[left]] -= 1
+            left += 1
+    a,b = result
+    print(a)
+    print(b)
 solution()
