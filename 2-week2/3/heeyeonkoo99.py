674b901b3345680463747bcf03902b27639c9982
Heeyeon Koo <rndus0819@naver.com>
2021-07-08 23:46:17 +0900

    dfs

---
+++ 2-week2/3/heeyeonkoo99.py
@@ -0,0 +1,32 @@
+#2주차 3
+#bfs/dfs문제이다.
+#여행경로
+
+def solution():
+    routes=dict()
+
+#그래프 생성
+    for i in range(int(input())):
+        start,end=map(str,input().split())
+        routes[start]=routes.get(start,[])+[end]
+
+    for r in routes.keys():
+        routes[r].sort(reverse=True)
+    
+    stack=["DCOM"]
+    path=[]
+    #print(routes)- 체크용
+    while stack:
+        top=stack[-1]
+    
+        if top not in routes or len(routes[top])==0:
+            path.append(stack.pop())
+        else:
+            stack.append(routes[top][-1])
+            routes[top]=routes[top][:-1]
+        
+    return path[::-1]
+
+solution()
+
+#Review)헷갈리면 https://gurumee92.tistory.com/165 사이트 다시한번 읽어보자
