04faa61780472b204df238cfc711626fb2903956
Jaehooni <8362532@gmail.com>
2021-07-08 15:39:51 +0000

    2-week2/2/Jaehooni.py

--- 2-week2/2/Jaehooni.py
+++ 2-week2/2/Jaehooni.py
@@ -4,58 +4,34 @@ parent = ['(',')']
 curly_bracket = ['{','}']
 square_bracket = ['[',']']
 
-def correct(bracket_deq):
-    parent_left = False
-    curly_left = False
-    square_left = False
+def correct(bracket_list):
+    parent_left = 0
+    curly_left = 0
+    square_left = 0
     
     for i in range(0, len(bracket_list)):
-        print(bracket_list[i])
-        
         if (bracket_list[i] == '('):
-            if (parent_left == True):
-                return False
-            
-            else:
-                parent_left == True
-            
+            parent_left += 1
+
         elif (bracket_list[i] == ')'):
-            if (parent_left == True):
-                parent_left == False
-            
-            else:
-                return False
-        
-        
-        if (bracket_list[i] == '{'):
-            if (curly_left == True):
-                return False
-            
-            else:
-                curly_left == True
-            
+            parent_left -= 1
+
+        elif (bracket_list[i] == '{'):
+            curly_left += 1
+
         elif (bracket_list[i] == '}'):
-            if (curly_left == True):
-                curly_left == False
-            
-            else:
-                return False
-            
-        if (bracket_list[i] == '['):
-            if (square_left == True):
-                return False
-            
-            else:
-                square_left == True
-            
-        elif (bracket_list[i] == ']'):
-            if (square_left == True):
-                square_left == False
-            
-            else:
-                return False
-            
-            
+            curly_left -= 1
+
+        elif (bracket_list[i] == '['):
+            square_left += 1
+
+        else:
+            square_left -= 1
+        
+        if (parent_left < 0 or curly_left < 0 or square_left < 0):
+            return False
+    
+    
     return True
             
 
@@ -68,7 +44,7 @@ for i in range(0, len(bracket_list)):
         # print(bracket_list)
         correct_num += 1
         
-    else:
+    # else:
         # print("uu" , bracket_list)
         
     rotate = bracket_list.popleft()
