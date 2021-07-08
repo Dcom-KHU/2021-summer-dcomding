b75053c582442978ed5e977a1faaa38268b94023
ZION <sju0924@khu.ac.kr>
2021-07-09 00:46:42 +0900

    Update sju0924.cpp

--- 2-week2/3/sju0924.cpp
+++ 2-week2/3/sju0924.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include<stack>
 #include<string>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
-string club[5001];
-string res[5001];
-vector<vector<pair<int,int>>> tree(5001);
-stack<int>st;
+pair<string, string> tickets[100001];
+bool visit[100001];
+vector<string> res;
 
-int get_number(string _c, int size);
-int search(int cur,int cnt);
+int search(string cur,int cnt);
 int N;
 int main()
 {
@@ -20,83 +18,44 @@ int main()
 	cin >> N;
 
 	for (int i = 0; i < N; i++) {
-		cin >> from>>to;
-		
-		nf = get_number(from, Size);
-		if (nf == -1) {
-			club[Size] = from;
-			nf =Size;
-			Size++;
-		}
-
-		nt = get_number(to, Size); 
-		if (nt == -1) {
-			club[Size] = to;
-			nt = Size;
-			Size++;
-		}
-		tree[nf].push_back(make_pair(nt,0));
-		
+		cin >> from>>to;		
+		tickets[i] = make_pair(from, to);		
 	}
+	sort(tickets, tickets + N);
 
 	int temp;
 	int start;
 
-	//이름순 내림차순으로 정렬하기
-	for (int i = 0; i < Size;i++) {
-		if (club[i] == "DCOM") start = i;
-		for (int j = 0; j < tree[i].size();j++) {
-			for (int k = j + 1; k < tree[i].size();k++) {
-				if (club[tree[i][j].first].length() > club[tree[i][k].first].length() || 
-					(club[tree[i][j].first].length() == club[tree[i][k].first].length()&&(club[tree[i][j].first] > club[tree[i][k].first]))) {//이름 빠른게 앞으로 가야 함
-					temp = tree[i][j].first;
-					tree[i][j].first = tree[i][k].first;
-					tree[i][k].first = temp;
-				}
-			}
-		}
-	}
-	search(start, 0);
+	
+	search("DCOM", 0);
 	for (int i = 0; i <= N; i++) {
 		cout << res[i] << " ";
 	}
 
 }
-
-int get_number(string _c, int size) {
-	int cnt = 0;
-	while (club[cnt] != _c && cnt < size) {
-		cnt++;
-	}
-
-	if (cnt == size) return -1;
-	return cnt;
-}
-int search(int cur,int cnt) {
+int search(string cur,int cnt) {
 	//cout << "cur: " << cur << " cnt : " << cnt << "\n";
 	bool is_searched=false, is_left = false;
 
 	if (cnt == N) {
-		res[N] = club[cur];
+		res.push_back(cur);
 		return true;
-	}
-	else if (cnt == N) {
-		return false;
 	}
 
 	
-	for (int i = 0;i < tree[cur].size();i++) {
-		if (tree[cur][i].second) continue;
-		is_left = true;
-		//res[cnt] = club[tree[cur][i].first];
-		tree[cur][i].second = 1;
-		is_searched = search(tree[cur][i].first, cnt + 1);
-		if (is_searched) {
-			res[cnt] = club[cur];
-			return true;
+	for (int i = 0;i <N;i++) {
+		if (visit[i] == false and cur == tickets[i].first) {
+			visit[i] = true;
+			res.push_back(cur);
+			is_searched = search(tickets[i].second, cnt + 1);
+			if (is_searched) {
+				return true;
+			}
+			visit[i] = false;
+			res.pop_back();
 		}
+		
 
-		tree[cur][i].second = 0;
 	}
 
 
