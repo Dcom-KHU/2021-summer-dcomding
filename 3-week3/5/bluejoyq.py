ff2bab9534dc2a80b1b3fa175836e80635e6da77
bluejoyq <bluejoyq@naver.com>
2021-07-11 15:44:36 +0000

    3-5 fix error for same value

--- 3-week3/5/bluejoyq.py
+++ 3-week3/5/bluejoyq.py
@@ -8,12 +8,13 @@ def solve():
     result = -1
     for i in range(1,N + 1):
         cur = values[i]
-        while values[stk[-1]] > cur:
+        while values[stk[-1]] >= cur:
             lst = stk.pop()
             result = max(result, (i - lst ) * values[lst], (lst - stk[-1] ) * values[lst] )
         stk.append(i)
     
     while len(stk) > 1:
+        
         lst = stk.pop()
         result = max(result, (N +1 - lst) * values[lst], (lst - stk[-1] ) * values[lst])
     
