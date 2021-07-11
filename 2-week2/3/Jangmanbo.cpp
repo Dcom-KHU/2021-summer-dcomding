bfecc9e9fd131e8753564defd052d1466964125f
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-11 22:53:23 +0900

    2-3

--- 2-week2/3/Jangmanbo.cpp
+++ 2-week2/3/Jangmanbo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int n;
 
-bool DFS(string start, vector<vector<string>> tickets, vector<int> stop, int consume, vector<string>& print) {
+bool DFS(string start, vector<vector<string>>& tickets, int consume, vector<string>& print) {
 	if (consume == n) { return true; }
 
 	int size = tickets.size();
@@ -14,15 +14,15 @@ bool DFS(string start, vector<vector<string>> tickets, vector<int> stop, int con
 
 	for (int i = 0; i < size; i++)
 	{
-		if (tickets[i][0] == start && !stop[i]) {
+		if (tickets[i][0] == start) {
 			buf = tickets[i];
 			print.push_back(buf[1]);
-			stop[i] = 1;
-			if (DFS(buf[1], tickets, stop, consume + 1, print)) {
+			tickets.erase(tickets.begin() + i);
+			if (DFS(buf[1], tickets, consume + 1, print)) {
 				return true;
 			}
+			tickets.insert(tickets.begin() + i, buf);
 			print.pop_back();
-			stop[i] = 0;
 		}
 	}
 
@@ -37,7 +37,6 @@ bool compare(vector<string> start, vector<string> end) {
 int main() {
 	cin >> n;
 	vector<vector<string>> tickets;
-	vector<int> stop(n, 0);
 	string depart, arrive;
 	for (int i = 0; i < n; i++)
 	{
@@ -47,7 +46,7 @@ int main() {
 	sort(tickets.begin(), tickets.end(), compare);
 
 	vector<string> print = { "DCOM" };
-	DFS("DCOM", tickets, stop, 0, print);
+	DFS("DCOM", tickets, 0, print);
 	for (int i = 0; i < n + 1; i++)
 	{
 		cout << print[i] << " ";
