e74167a7a7f962e959d2e503cfd01f54922bc4db
all-eviate <riverboy94@gmail.com>
2021-07-11 22:25:16 +0900

    W2-Q5

--- 2-week2/5/all-eviate.py
+++ 2-week2/5/all-eviate.py
@@ -3,38 +3,35 @@ n = int(input(""))
 directions = list(map(int, input("").split()))
 
 new = True
-now = [0, 0]
-visited = []
-visited.append(deepcopy(now))
+now = (0, 0)
+visited = {now:[-1]}
 shapes = 0
 
 for d in directions:
     if d == 0:
-        now[1] += 1
+        nex = (now[0], now[1]+1)
     elif d == 1:
-        now[0] += 1
-        now[1] += 1
+        nex = (now[0]+1, now[1]+1)
     elif d == 2:
-        now[0] += 1
+        nex = (now[0]+1, now[1])
     elif d == 3:
-        now[0] += 1
-        now[1] -= 1
+        nex = (now[0]+1, now[1]-1)
     elif d == 4:
-        now[1] -= 1
+        nex = (now[0], now[1]-1)
     elif d == 5:
-        now[0] -= 1
-        now[1] -= 1
+        nex = (now[0]-1, now[1]-1)
     elif d == 6:
-        now[0] -= 1
+        nex = (now[0]-1, now[1])
     elif d == 7:
-        now[0] -= 1
-        now[1] += 1
-    if now in visited:
-        if new:
+        nex = (now[0]-1, now[1]+1)
+    if nex in visited:
+        if d not in visited[nex]:
             shapes += 1
+            visited[nex].append(d)
         new = False
     else:
-        visited.append(deepcopy(now))
+        visited[nex] = [d]
         new = True
+    now = nex
 
 print(shapes)
