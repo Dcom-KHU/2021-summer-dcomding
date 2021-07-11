61020824c6ee29eba00b25894bef947831d8f449
hyeweon <berry_na@khu.ac.kr>
2021-07-11 15:23:11 +0900

    Create hyeweon.cpp
    2-week2

---
+++ 2-week2/4/hyeweon.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	string* items = new string[n];
+	string types[10];
+	int count[10];
+	int types_index = 0;
+	bool newtype = true;
+
+	int start = 0;
+	int end = 0;
+	int min_start = 0;
+	int min_end = n;
+	bool zerotype = false;
+
+	for (int i = 0; i < n; i++) {
+		cin >> items[i];
+		for (int j = 0; j < types_index; j++) {
+			if (items[i] == types[j]) {
+				//count[j]++;
+				newtype = false;
+				break;
+			}
+		}
+		if (newtype == true) {
+			types[types_index] = items[i];
+			count[types_index] = 0;
+			types_index++;
+		}
+		//end++;
+		newtype = true;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < types_index; j++) {
+			if (items[i] == types[j]) {
+				count[j]++;
+				break;
+			}
+		}
+
+		for (int j = 0; j < types_index; j++) {
+			if (count[j] == 0) {
+				zerotype = true;
+				break;
+			}
+		}
+		if (zerotype == false) {
+			for (int j = 0; j < types_index; j++) {
+				if (items[start] == types[j]) {
+					if (count[j] > 1) {
+						count[j]--;
+						start++;
+						j = 0;
+					}
+				}
+			}
+
+			if (min_end - min_start > end - start) {
+				min_start = start;
+				min_end = end;
+			}
+		}
+		zerotype = false;
+		end++;
+	}
+
+	cout << min_start + 1 << endl;
+	cout << min_end + 1 << endl;
+}
