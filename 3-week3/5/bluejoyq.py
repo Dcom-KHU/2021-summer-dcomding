95cee5aea7038847182ed011164c1c9bff2dc112
bluejoyq <bluejoyq@naver.com>
2021-07-11 16:29:30 +0000

    3-5 fix have both side width

--- 3-week3/5/bluejoyq.py
+++ 3-week3/5/bluejoyq.py
@@ -1,23 +1,20 @@
 def solve():
-    N = int(input())
+    N = int(input()) +1 
     values = list(map(int, input().split()))
     values.insert(0,-1)
-    
     # -1 기록
     stk = [0]
     result = -1
-    for i in range(1,N + 1):
+    for i in range(1,N):
         cur = values[i]
         while values[stk[-1]] >= cur:
             lst = stk.pop()
-            result = max(result, (i - lst ) * values[lst], (lst - stk[-1] ) * values[lst] )
+            result = max(result, (i - stk[-1] - 1) * values[lst] )
         stk.append(i)
     
     while len(stk) > 1:
-        
         lst = stk.pop()
-        result = max(result, (N +1 - lst) * values[lst], (lst - stk[-1] ) * values[lst])
-    
+        result = max(result, (N - stk[-1] -1) * values[lst])
+        
     print(result)
-
 solve()
