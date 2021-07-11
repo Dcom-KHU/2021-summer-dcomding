73b951b3e126e52dd4041d1a20b14d82f9742584
all-eviate <riverboy94@gmail.com>
2021-07-11 21:38:47 +0900

    W2-Q4

--- 2-week2/4/all-eviate.py
+++ 2-week2/4/all-eviate.py
@@ -3,23 +3,57 @@ apples = []
 for i in range(n):
     apples.append(input(""))
 
-products = len(set(apples)) # total apple products
+products = set(apples) # total apple products
+np = len(products)
+
+optimum = -1
+
+basket = {}
+basketset = set()
+
+start = 0
+end = 0
 
 buyfrom = 0
 buyto = 0
 
-for counts in range(products, n+1):
-    if buyfrom != 0 and buyto != 0:
-        break
-    basket = []
-    for start in range(0, n-products+1):
-        end = start + counts
-        basket = apples[start:end]
-        basketset = set(basket)
-        if len(basketset) == products:
-            buyfrom = start + 1
-            buyto = end
+counts = 0
+
+for start in range(n):
+    done = False
+
+    for end in range(counts, n):
+        try:
+            basket[apples[end]] += 1
+        except:
+            basket[apples[end]] = 1
+            basketset.add(apples[end])
+
+        if np == len(basketset):
+            if optimum < 0 or (end - start) < optimum:
+                optimum = end - start
+                buyfrom = start + 1
+                buyto = end + 1
+            
+            done = True
+            counts = end
             break
+    if not done:
+        break
+
+    if apples[start] in basket.keys():
+        if basket[apples[start]] == 1:
+            del basket[apples[start]]
+            basketset.remove(apples[start])
+        else:
+            basket[apples[start]] -= 1
+    
+    if apples[end] in basket.keys():
+        if basket[apples[end]] == 1:
+            del basket[apples[end]]
+            basketset.remove(apples[end])
+        else:
+            basket[apples[end]] -= 1
 
 print(buyfrom)
 print(buyto)
