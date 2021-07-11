7e7cac7102ab385ef07e4a3a83d77b25730be37e
FacerAin <ywsong.dev@kakao.com>
2021-07-11 00:21:00 +0000

    2-4

--- 2-week2/4/FacerAin.cpp
+++ 2-week2/4/FacerAin.cpp
@@ -4,8 +4,7 @@
 #include <set>
 using namespace std;
 
-//IPHONE, MACPRO, MACAIR, AIRPODSPRO
-int cnt_arr[] = {0,0,0,0};
+int n;
 int item_cnt = 0;
 int category_cnt;
 vector<string> v;
@@ -16,7 +15,7 @@ set<string> s;
 
 void get_start_window(){
 	int isFind = false;
-	int idx = 0;
+	int idx = s_idx;
 	while(!isFind){
 		m[v[idx]] -= 1;
 		if(m[v[idx]] == 0){
@@ -24,6 +23,7 @@ void get_start_window(){
 		}
 		if(item_cnt != category_cnt){
 			m[v[idx]] += 1;
+			item_cnt += 1;
 			s_idx = idx;
 			isFind = true;
 		}
@@ -33,7 +33,7 @@ void get_start_window(){
 
 void get_end_window(){
 	bool isFind = false;
-	int idx = 0;
+	int idx = e_idx;
 	while(!isFind){
 		m[v[idx]] += 1;
 		if(m[v[idx]] == 1){
@@ -50,7 +50,7 @@ void get_end_window(){
 int main(){
 
 	int s_ans, e_ans, w_size;
-	int n;
+
 	bool isBuy = false;
 	cin >> n;
 	for(int i = 0; i < n; i++){
@@ -64,33 +64,27 @@ int main(){
 	category_cnt = m.size();
 	//슬라이딩 윈도우의 끝 지점을 찾는다.
 	get_end_window();
+	
 	//슬라이딩 윈도우의 시작 지점을 찾는다.
 	get_start_window();
-	
 	
 	s_ans = s_idx;
 	e_ans = e_idx;
 	w_size = e_idx - s_idx + 1;
-	
 	while(e_idx < n-1){
 		e_idx++;
-		s_idx++;
 		m[v[e_idx]] += 1;
+		
 		if(m[v[e_idx]] == 1){
 			item_cnt += 1;
 		}
-		m[v[s_idx]] -= 1;
-		if(m[v[s_idx]] == 0){
-			item_cnt -= 1;
-		}
 		
-		if(item_cnt == category_cnt){
+
 			get_start_window();
 			if(w_size > e_idx - s_idx + 1){
 					s_ans = s_idx;
 					e_ans = e_idx;
 			}
-		}
 		
 		
 		
