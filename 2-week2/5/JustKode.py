15ed9896d14414bef2060fcd5887a4b4c72b24e5
justkode <sobu0715@gmail.com>
2021-07-11 23:34:37 +0900

    fix: 02-05 #3

--- 2-week2/5/JustKode.py
+++ 2-week2/5/JustKode.py
@@ -1,6 +1,7 @@
 n = int(input())
 list_var = list(map(int, input().split()))
-set_var = {(0, 0), }
+set_var = {(0, 0)}
+edge_var = set()
 
 dx = [0, 1, 1, 1, 0, -1, -1, -1]
 dy = [1, 1, 0, -1, -1, -1, 0, 1]
@@ -8,22 +9,31 @@ dy = [1, 1, 0, -1, -1, -1, 0, 1]
 count = 0
 current = [0, 0]
 
-def dfs(depth):
-    global current
-    global count
+for depth in range(n):
+    nx = dx[list_var[depth]]
+    ny = dy[list_var[depth]]
 
-    if depth == n:
-        return
+    first = tuple(sorted([(current[0], current[1]), (current[0] + nx, current[1] + ny)]))
+    second = tuple(sorted([(current[0] + nx, current[1] + ny), (current[0] + 2 * nx, current[1] + 2 * ny)]))
 
-    current[0] += dx[list_var[depth]]
-    current[1] += dy[list_var[depth]]
+    current[0] += nx
+    current[1] += ny
 
-    if tuple(current) in set_var:
-        count += 1
-    else:
-        set_var.add(tuple(current))
+    if first not in edge_var:
+        edge_var.add(first)
+        if tuple(current) in set_var:
+            count += 1
+        else:
+            set_var.add(tuple(current))
     
-    dfs(depth + 1)
+    current[0] += nx
+    current[1] += ny
 
-dfs(0)
+    if second not in edge_var:
+        edge_var.add(second)
+        if tuple(current) in set_var:
+            count += 1
+        else:
+            set_var.add(tuple(current))
+    
 print(count)
