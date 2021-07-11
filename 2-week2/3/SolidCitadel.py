bed6f3d62e5a470286116b3eecc9356f9c7627c0
SolidCitadel <teddy3582@naver.com>
2021-07-11 22:09:13 +0900

    Update SolidCitadel.py

--- 2-week2/3/SolidCitadel.py
+++ 2-week2/3/SolidCitadel.py
@@ -12,14 +12,12 @@ groups = {k: sorted(sorted(v), key=lambda x: len(x)) for k, v in groups.items()}
 route = [('start', 0)]
 
 queue = deque([route])
-while len(queue):
+while len(route) < n+1:
     route = queue.popleft()
     f = groups[route[-1][0]][route[-1][1]]
     if f in groups.keys():
         for i in range(len(groups[f])):
             if (f, i) not in route:
                 queue.append((route+[(f, i)]))
-    if len(route) == n+1:
-        break
-    
+
 print(' '.join([groups[f][i] for f, i in route]))
