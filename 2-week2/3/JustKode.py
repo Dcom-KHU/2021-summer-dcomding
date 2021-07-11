303adfdd08daa4ba2cccf105b7fdf0e4ad0e2003
justkode <sobu0715@gmail.com>
2021-07-11 19:06:27 +0900

    fix: 02-03 #2

--- 2-week2/3/JustKode.py
+++ 2-week2/3/JustKode.py
@@ -1,22 +1,36 @@
 n = int(input())
 dict_var = {}
+current = "DCOM"
+result = ["DCOM"]
+check = False
+
+def func(key):
+    global dict_var, current, result, check, n
+    if check:
+        return
+
+    if len(result) == n + 1:
+        print(' '.join(result))
+        check = True
+        return
+        
+
+    for i in range(len(dict_var[key])):
+        if not dict_var[key][i][0]:
+            dict_var[key][i][0] = True
+            result.append(dict_var[key][i][1])
+            func(dict_var[key][i][1])
+            result.pop()
+            dict_var[key][i][0] = False
 
 for i in range(n):
     start, end = input().split()
     if start not in dict_var:
-        dict_var[start] = [end]
+        dict_var[start] = [[False, end]]
     else:
-        dict_var[start].append(end)
+        dict_var[start].append([False, end])
 
 for key in dict_var:
-    dict_var[key].sort(key=lambda x: (len(x), x))
-    dict_var[key].reverse()
+    dict_var[key].sort(key=lambda x: (len(x[1]), x[1]))
 
-current = "DCOM"
-result = "DCOM"
-
-for i in range(n):
-    current = dict_var[current].pop()
-    result += ' ' + current
-
-print(result)
+func("DCOM")
