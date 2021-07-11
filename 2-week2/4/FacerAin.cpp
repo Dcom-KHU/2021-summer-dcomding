57ba9310f6035290e4e47d455c73f3cc0530bbbb
FacerAin <ywsong.dev@kakao.com>
2021-07-11 06:18:24 +0000

    2-4

--- 2-week2/4/FacerAin.cpp
+++ 2-week2/4/FacerAin.cpp
@@ -13,6 +13,7 @@ int e_idx = 0;
 map<string, int> m;
 set<string> s;
 
+/*
 void get_start_window(){
 	int isFind = false;
 	int idx = s_idx;
@@ -30,11 +31,13 @@ void get_start_window(){
 		idx++;
 	}
 }
+*/
 
 void get_end_window(){
 	bool isFind = false;
 	int idx = e_idx;
-	while(!isFind){
+	while(!isFind && idx < n){
+		//cout << idx << endl;
 		m[v[idx]] += 1;
 		if(m[v[idx]] == 1){
 			item_cnt += 1;
@@ -64,26 +67,26 @@ int main(){
 	category_cnt = m.size();
 	//슬라이딩 윈도우의 끝 지점을 찾는다.
 	get_end_window();
-	
-	//슬라이딩 윈도우의 시작 지점을 찾는다.
-	get_start_window();
-	
 	s_ans = s_idx;
 	e_ans = e_idx;
-	w_size = e_idx - s_idx + 1;
-	while(e_idx < n-1){
-		e_idx++;
-		m[v[e_idx]] += 1;
-		if(m[v[e_idx]] == 1){
-			item_cnt += 1;
+	w_size = e_ans - s_ans;
+	while(e_idx < n){
+		//cout << s_idx << " " << e_idx << endl;
+		string item = v[s_idx];
+		m[item]--;
+		s_idx++;
+		if(m[item] == 0){
+			item_cnt--;
+			e_idx++;
+			get_end_window();
 		}
-		if(item_cnt == category_cnt){
-			get_start_window();
-				if(w_size > e_idx - s_idx + 1){
+		
+		
+		if(item_cnt == category_cnt && w_size > e_idx - s_idx){
 					s_ans = s_idx;
 					e_ans = e_idx;
+					w_size =  e_ans - s_ans;
 			}	
-		}
 
 		
 	}
