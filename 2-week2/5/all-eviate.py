f36bd31951d0989a2577e8a0f9c361547f6c8466
all-eviate <riverboy94@gmail.com>
2021-07-11 22:45:26 +0900

    W2-Q5

--- 2-week2/5/all-eviate.py
+++ 2-week2/5/all-eviate.py
@@ -6,28 +6,60 @@ new = True
 now = (0, 0)
 visited = {now:[-1]}
 shapes = 0
+additional = False
 
 for d in directions:
     if d == 0:
         nex = (now[0], now[1]+1)
     elif d == 1:
         nex = (now[0]+1, now[1]+1)
+        try:
+            if 7 in visited[(now[0]), now[1]+1] or 3 in visited[(now[0]+1, now[1])]:
+                additional = True
+            else:
+                additional = False
+        except:
+            additional = False
     elif d == 2:
         nex = (now[0]+1, now[1])
     elif d == 3:
         nex = (now[0]+1, now[1]-1)
+        try:
+            if 5 in visited[(now[0], now[1]-1)] or 1 in visited[(now[0]+1, now[1])]:
+                additional = True
+            else:
+                additional = False
+        except:
+            additional = False
     elif d == 4:
         nex = (now[0], now[1]-1)
     elif d == 5:
         nex = (now[0]-1, now[1]-1)
+        try:
+            if 7 in visited[(now[0]-1, now[1])] or 3 in visited[(now[0], now[1]-1)]:
+                additional = True
+            else:
+                additional = False
+        except:
+            additional = False
     elif d == 6:
         nex = (now[0]-1, now[1])
     elif d == 7:
         nex = (now[0]-1, now[1]+1)
+        try:
+            if 5 in visited[(now[0]-1, now[1])] or 1 in visited[(now[0], now[1]+1)]:
+                additional = True
+            else:
+                additional = False
+        except:
+            additional = False
     if nex in visited:
         if d not in visited[nex]:
             shapes += 1
             visited[nex].append(d)
+            if additional:
+                shapes += 1
+                additional = False
         new = False
     else:
         visited[nex] = [d]
