95597f9f9bc631a2ef812d5c164da2b2545339e5
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-07-12 08:33:17 +0000

    stack -> recursive function

--- 3-week3/4/Untaini.cpp
+++ 3-week3/4/Untaini.cpp
@@ -1,11 +1,9 @@
 #include <cstdio>
 #include <vector>
-#include <stack>
 #include <algorithm>
 using namespace std;
 
-int campus[11][11], n, m, dx[]={1,-1,0,0}, dy[]={0,0,1,-1}, bMinPos[7][2], bMaxPos[7][2], group[7], result;
-stack<pair<int, int>> funcStack;
+int campus[11][11], n, m;
 vector<pair<int, pair<int, int>>> bridges;
 
 //2차원 배열의 유효한 위치인 지 확인하는 함수
@@ -13,11 +11,45 @@ bool canGotoPos(int x, int y){
 	return 0<=x && x<n && 0<=y && y<m;
 }
 
+//dfs 건물, 다리 탐색용
+int dx[]={1,-1,0,0}, dy[]={0,0,1,-1}, bCnt;
+void dfs(int x, int y, int mode, int val){
+	
+	//건물 탐색용
+	if(mode == -1){
+		campus[x][y] = val;
+		
+		for(int cnt=0;cnt<4;++cnt){
+			int nx = x+dx[cnt], ny = y+dy[cnt];
+			if(canGotoPos(nx, ny) && campus[nx][ny] == -1)
+				dfs(nx, ny, mode, val);
+		}
+	}
+	
+	//다리 탐색용
+	else{
+		int nx = x+dx[mode], ny = y+dy[mode];
+		if(canGotoPos(nx, ny)){
+			
+			//다음 지점이 빈 공간이면 한칸 더감
+			if(campus[nx][ny] == 0)
+				dfs(nx, ny, mode, val+1);
+			
+			//다음 지점이 자기 건물이 아니면서 거리가 1보다 크면 벡터에 저장
+			else if(campus[nx][ny] != bCnt && val>1)
+				bridges.push_back(make_pair(val, make_pair(min(bCnt, campus[nx][ny]), max(bCnt, campus[nx][ny]))));
+			
+		}
+	}
+}
+
 //union-find 함수
+int group[7];
 int findGroup(int bNum){
 	if(bNum == group[bNum]) return bNum;
 	return group[bNum] = findGroup(group[bNum]);
 }
+
  
 int main() {
 	scanf("%d%d",&n,&m);
@@ -32,76 +64,32 @@ int main() {
 	int buildingCnt = 0;
 	for(int nCnt = 0; nCnt<n; ++nCnt)
 		for(int mCnt = 0; mCnt<m; ++mCnt)
-			if(campus[nCnt][mCnt] == -1) {
-				++buildingCnt;
-				bMinPos[buildingCnt][0] = bMinPos[buildingCnt][1] = 11;
-				funcStack.push(make_pair(nCnt,mCnt));
-				
-				while(!funcStack.empty()){
-					int x = funcStack.top().first, y = funcStack.top().second; funcStack.pop();
-					campus[x][y] = buildingCnt;
-					
-					//아래 for문 탐색을 최소화 하기 위한 건물의 사각 영역 갱신
-					bMinPos[buildingCnt][0] = min(x, bMinPos[buildingCnt][0]);
-					bMinPos[buildingCnt][1] = min(y, bMinPos[buildingCnt][1]);
-					bMaxPos[buildingCnt][0] = max(x, bMaxPos[buildingCnt][0]);
-					bMaxPos[buildingCnt][1] = max(y, bMaxPos[buildingCnt][1]);
-					
-					for(int cnt=0;cnt<4;++cnt){
-						int nx = x+dx[cnt], ny = y+dy[cnt];
-						if(canGotoPos(nx, ny) && campus[nx][ny] == -1)
-							funcStack.push(make_pair(nx,ny));
-					}
-				}
-			}
+			
+			//방문한 지점에 번호가 매겨지지 않은 건물이 있으면 건물에 번호를 붙여줌
+			if(campus[nCnt][mCnt] == -1) 
+				dfs(nCnt, mCnt, -1, ++buildingCnt);
+			
 	
-	//dfs를 통해 각 건물을 연결하는 다리 만들기 (10: 오른쪽, 20: 왼쪽, 30: 아래, 40: 위 / 일의자리 숫자가 거리)
-	for(int bCnt = 1; bCnt<=buildingCnt; ++bCnt)
-		
-		//xCnt, yCnt는 for문을 최대한 적게 돌기 위한 처리
-		for(int xCnt = bMinPos[bCnt][0]; xCnt<=bMaxPos[bCnt][0]; ++xCnt)
-			for(int yCnt = bMinPos[bCnt][1]; yCnt<=bMaxPos[bCnt][1]; ++yCnt)
+	//dfs를 통해 각 건물을 연결하는 다리 만들기 bCnt는 dfs()에서도 사용됨
+	for(bCnt = 1; bCnt<=buildingCnt; ++bCnt)
+		for(int nCnt = 0; nCnt<n; ++nCnt)
+			for(int mCnt = 0; mCnt<m; ++mCnt)
 				
-				//방문한 지점이 건물이면
-				if(campus[xCnt][yCnt] == bCnt){
-					for(int cnt=0; cnt<4; ++cnt){
-						int tx = xCnt+dx[cnt], ty = yCnt+dy[cnt];
-						
-						//다음 지점이 빈 공간인 것 확인하기
-						if(canGotoPos(tx, ty) && campus[tx][ty] == 0){
-							campus[tx][ty] = cnt*10+11;
-							funcStack.push(make_pair(tx, ty));
-							
-							//한 방향으로 끝까지 가기 위한 스택
-							while(!funcStack.empty()){
-								int x = funcStack.top().first, y = funcStack.top().second, val = campus[x][y]; funcStack.pop();
-								int nx = x+dx[val/10-1], ny = y+dy[val/10-1];
-								if(canGotoPos(nx, ny)){
-									//다음 지점이 빈 공간이면 한칸 더감
-									if(campus[nx][ny] == 0){
-										funcStack.push(make_pair(nx, ny));
-										campus[nx][ny] = val+1;
-									}
-									//다음 지점이 자기 건물이 아니면서 거리가 1보다 크면 벡터에 저장
-									else if(campus[nx][ny] != bCnt && val%10>1)
-										bridges.push_back(make_pair(val%10, make_pair(min(bCnt, campus[nx][ny]), max(bCnt, campus[nx][ny]))));
-										
-								}
-								//다음 다리 탐색을 위한 다리 기록 초기화
-								campus[x][y] = 0;
-							}
-						}
-					}
-				}
+				//방문한 지점이 건물이면 한 방향으로 다리를 놓아봄
+				if(campus[nCnt][mCnt] == bCnt)
+					for(int cnt=0; cnt<4; ++cnt)
+						dfs(nCnt, mCnt, cnt, 0);
+				
 	
 	//unoin-find 초기 세팅
 	for(int cnt=1; cnt<=6; ++cnt)
 		group[cnt] = cnt;
+
 	sort(bridges.begin(), bridges.end());
 	bridges.erase(unique(bridges.begin(), bridges.end()),bridges.end());
-
+	
 	//최소 스패닝 트리를 이용한 간선 채택
-	int matchCnt=1;
+	int matchCnt=1, result=0;
 	for(int cnt=0; cnt<bridges.size(); ++cnt){
 		int val = bridges[cnt].first, x = bridges[cnt].second.first, y = bridges[cnt].second.second;
 		
