37160bb13afeda8a3a40fe786840f421b1bf4dfa
Jinsu Park <dev.umijs@gmail.com>
2021-07-12 10:43:51 +0900

    Jinsu

--- 3-week3/1/umi0410.py
+++ 3-week3/1/umi0410.py
@@ -1,9 +1,9 @@
 # 조합 후 set 한 뒤 각
-def solution(nums):
-    MAX = len(nums) / 2
+def solution(nums, MAX):
     numOfKinds = len(set(nums))
     return int(min(MAX, numOfKinds))
-
-print(solution([3,1,2,3]))
-print(solution([3,3,3,2,2,4]))
-print(solution([3,3,3,2,2,2]))
+_, MAX = map(int, input().split())
+print(solution(map(int, input().split()), MAX))
+# print(solution([3,1,2,3]))
+# print(solution([3,3,3,2,2,4]))
+# print(solution([3,3,3,2,2,2]))
