9e2feadaca70a959b4f99c40194979343658688f
Heeyeon Koo <rndus0819@naver.com>
2021-07-14 13:18:04 +0900

    3주차-1

---
+++ 3-week3/1/heeyeonkoo99.py
@@ -0,0 +1,14 @@
+#3주차-1
+n,k=map(int,input().split())
+nums=list(map(int,input().split()))
+def solution(nums):
+    answer=0
+    length=len(nums)//2
+    temp=list(set(nums))
+
+    for value in temp:
+        if answer<length:
+            answer+=1
+    return answer
+print(solution(nums))
+#항상 어떻게 하면 더 단순하고 직관적으로 구현할수 있을지 생각하자.
