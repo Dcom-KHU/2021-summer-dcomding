49bea7806f95c4a475b19dc5306733c84effc24d
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-15 02:13:50 +0900

    3-5

--- 3-week3/5/Jangmanbo.cpp
+++ 3-week3/5/Jangmanbo.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
+#include <stack>
+#include <algorithm>
 using namespace std;
 
-void max_area(int end, int idx, int& width, int& height, int heights[], int& max) {
-	if (idx + width > end) {
-		height = 1000001;
-		width = 1;
-		return;
-	}
-
-	int area;
-	
-	if (heights[idx + width - 1] < height)
-	{
-		height = heights[idx + width - 1];
-
-	}
-	area = width * height;
-	if (area > max) {
-		max = area;
-	}
-	max_area(end, idx, ++width, height, heights, max);
-}
-
 int main() {
-	int n;
+	stack<int> idx;
+	int n, height, width, area = 0;
 	cin >> n;
 	int* heights = new int[n];
 	for (int i = 0; i < n; i++)
@@ -31,13 +13,29 @@ int main() {
 		cin >> heights[i];
 	}
 
-	int max = 0, width = 1, height = 100001;
-	for (int i = 0; i < n; i++)
+	bool loop = true;
+	for (int i = 0; i <= n; i++)
 	{
-		max_area(n, i, width, height, heights, max);
+		if (!idx.empty() && heights[idx.top()] > heights[i])
+		{
+			do
+			{
+				height = heights[idx.top()];
+				idx.pop();
+				if (idx.empty())
+				{
+					width = i;
+					loop = false;
+				}
+				else width = i - idx.top() - 1;
+				area = max(area, height * width);
+			} while (loop);
+			loop = true;
+		}
+		idx.push(i);
 	}
-	
-	cout << max;
+
+	cout << area;
 
 	return 0;
 }
