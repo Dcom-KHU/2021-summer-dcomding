4e5e8150753d8668fa52ab44f65cfed8b9dff79e
KMileStone <tdj03204@naver.com>
2021-07-15 11:31:56 +0900

    submit 3-week3/2

---
+++ 3-week3/2/KMileStone.py
@@ -0,0 +1,19 @@
+n = int(input())
+table = []
+for i in range(n):
+    table.append(list(map(int, input().split())))
+
+
+# DP : d[i+table[i][0]-1] = max(d[i-1]+table[i][1], d[i+table[i][0]-1])
+d = [0 for i in range(n)]
+
+for i in range(n):
+    due_date = i+table[i][0]-1
+
+    if due_date < n:
+        d[due_date] = max(d[i-1]+table[i][1], d[due_date])
+
+    if d[i] < d[i-1]:
+        d[i] = d[i-1]
+
+print(d[n-1])
