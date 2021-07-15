aca50d7487d9c4c02eec35612564809764fd153b
KMileStone <tdj03204@naver.com>
2021-07-15 13:39:31 +0900

    submit 3-week3/5

--- 3-week3/5/KMileStone.py
+++ 3-week3/5/KMileStone.py
@@ -7,20 +7,9 @@ def divide(seq):
     global max_area
     mid = min(seq)
     idx = seq.index(mid)
-    area = 0
+    area = mid * len(seq)
 
-    for h in heights:
-        # if mid <= h, you can make rectangle
-        if mid <= h:
-            area += mid
-        else:
-            # update max
-            if area > max_area:
-                max_area = area
-
-            area = 0
-
-    # update max with last rectangle
+    # update max
     if area > max_area:
         max_area = area
 
