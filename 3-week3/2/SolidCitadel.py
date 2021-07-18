9c879b71ad474dd60e06e684cb1d8d285d4f2bf4
SolidCitadel <teddy3582@naver.com>
2021-07-18 17:36:08 +0900

    Update SolidCitadel.py

--- 3-week3/2/SolidCitadel.py
+++ 3-week3/2/SolidCitadel.py
@@ -3,9 +3,7 @@ def fn(schedule, day):
     for i, (t, p) in enumerate(TP[day-1:]):
         i += day-1
         if sum(schedule[i:i+t]) == 0 and i+t <= n:
-            ns = schedule[:]
-            ns[i:i+t] = [i+1]*t
-            fn(ns, i+t+1)
+            fn(schedule[:i] + [i+1]*t + schedule[i+t:], i+t+1)
     global maxi
     maxi = max(sum([TP[i-1][1] for i in set(schedule) if i]), maxi)
 
