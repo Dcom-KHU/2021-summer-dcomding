e51f3fd6c8b14ab67526d814b44414a5c817cc42
hyeweon <berry_na@khu.ac.kr>
2021-07-18 20:17:54 +0900

    Create hyeweon.cpp
    2-week3

---
+++ 3-week3/2/hyeweon.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int* t = new int[n + 1];
+	int* p = new int[n + 1];
+	for (int i = 0; i < n; i++) {
+		cin >> t[i] >> p[i];
+	}
+	t[n] = p[n] = 0;
+	
+	int p_sum = 0;
+	for (int i = n - 1; i >= 0; i--) {
+		if (i + t[i] > n)
+			p[i] = p_sum;
+		else {
+			p[i] = max(p[i + 1], p[i] + p[i + t[i]]);
+			p_sum = max(p_sum, p[i]);
+		}
+	}
+	cout << p_sum << endl;
+
+	return 0;
+}
