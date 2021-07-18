af23641430298df3b939df64fef142fd28b85996
박민재 <sobu0715@gmail.com>
2021-07-18 18:09:43 +0900

    fix: 3-5 #3

--- 3-week3/4/JustKode.py
+++ 3-week3/4/JustKode.py
@@ -7,6 +7,7 @@ group_key_index = 0
 group_key = ['A', 'B', 'C', 'D', 'E', 'F']
 group = {}
 road = {}
+loc_to_group = {}
 
 
 def dfs(x, y, key):
@@ -25,22 +26,33 @@ def dfs(x, y, key):
         if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1:
             if (nx, ny) not in group[key]:
                 group[key].append((nx, ny))
+                loc_to_group[(nx, ny)] = key
                 dfs(nx, ny, key)
 
 
-def find_road(x, y, dx, dy, start, depth):
+def find_road(x, y, dx, dy, current, depth):
     nx = x + dx
     ny = y + dy
 
     if 0 <= nx < n and 0 <= ny < m:
-        for key in group:
-            if key == start:
-                continue
+        if arr[nx][ny] == 0:
+            if current == 'null':
+                find_road(nx, ny, dx, dy, current, depth)
+            else:
+                find_road(nx, ny, dx, dy, current, depth + 1)
+        else:
+            if current == 'null':
+                find_road(nx, ny, dx, dy, loc_to_group[(nx, ny)], depth)
+            else:
+                if loc_to_group[(nx, ny)] == current:
+                    find_road(nx, ny, dx, dy, current, depth)
+                elif depth >= 2:
+                    road[current][loc_to_group[(nx, ny)]] = depth
+                    road[loc_to_group[(nx, ny)]][current] = depth
+                    find_road(nx, ny, dx, dy, loc_to_group[(nx, ny)], 0)
+                else:
+                    find_road(nx, ny, dx, dy, loc_to_group[(nx, ny)], 0)
 
-            if arr[nx][ny] == 0:
-                find_road(nx, ny, dx, dy, start, depth + 1)
-            elif (nx, ny) in group[key] and depth >= 2:
-                road[start][key] = min(road[start][key], depth)
 
 
 
@@ -48,6 +60,7 @@ for i in range(n):
     for j in range(m):
         if arr[i][j] == 1 and (i, j) not in checked:
             key = group_key[group_key_index]
+            loc_to_group[(i, j)] = key
             group[key] = [(i, j)]
             dfs(i, j, key)
             group_key_index += 1
@@ -61,20 +74,19 @@ for key in group:
             continue
         road[key][key_2] = 1000
 
-for key in group:
-    for element in group[key]:
-        dx = [-1, 0, 1, 0]
-        dy = [0, 1, 0, -1]
+# x에 대한 길
+for i in range(n):
+    if (i, 0) in loc_to_group:
+        find_road(i, 0, 0, 1, loc_to_group[(i, 0)], 0)
+    else:
+        find_road(i, 0, 0, 1, 'null', 0)
 
-        for i in range(4):
-            nx = element[0] + dx[i]
-            ny = element[1] + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m:
-                find_road(element[0], element[1], dx[i], dy[i], key, 0)
-
-
-
+# y에 대한 길
+for i in range(m):
+    if (0, i) in loc_to_group:
+        find_road(0, i, 1, 0, loc_to_group[(0, i)], 0)
+    else:
+        find_road(0, i, 1, 0, 'null', 0)
 
 graph = []
 
@@ -102,12 +114,14 @@ cost = 0
 while True:
     if edges == len(group) - 1:
         break
+
     u, v, weight = graph.pop(0)
 
     if find(u) != find(v):
         union(u, v)
         cost += weight
         edges += 1
+        print(cost)
 
 if cost >= 1000:
     print(-1)
