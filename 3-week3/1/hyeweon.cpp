059c71a172579c57f3d4c71f0816ff29bf75ed8b
hyeweon <berry_na@khu.ac.kr>
2021-07-18 19:22:11 +0900

    Create hyeweon.cpp
    1-week3

---
+++ 3-week3/1/hyeweon.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int* books = new int[n];
+	vector<int> booktypes;
+
+	for (int i = 0; i < n; i++) {
+		cin >> books[i];
+		if (find(booktypes.begin(), booktypes.end(), books[i]) == booktypes.end())
+			booktypes.push_back(books[i]);
+	}
+	
+	int type = booktypes.size();
+	if (k > type)
+		cout << type << endl;
+	else
+		cout << k << endl;
+
+	return 0;
+}
