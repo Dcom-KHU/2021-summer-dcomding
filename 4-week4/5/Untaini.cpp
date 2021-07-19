3db30d279847a55705ba4b46fdafc34d3cd6f982
Untaini <55027231+Untaini@users.noreply.github.com>
2021-07-19 19:48:49 +0900

    edit variable n

--- 4-week4/5/Untaini.cpp
+++ 4-week4/5/Untaini.cpp
@@ -5,19 +5,19 @@
 int n, v, fenwickTree[100010], distanceFromRoot[100010], result;
 
 void update(int num, int val){
-    while(num<=n+2)
+    while(num<=n)
         fenwickTree[num] += val, num += (num & -num);
 }
 
 int sumOneToNum(int num){
     int res = 0;
-    while(num>0)
+    while(num)
         res += fenwickTree[num], num -= (num & -num);
     return res;
 }
 
 int binarySearch(int val){
-    int left = 1, right = n+2, mid;
+    int left = 1, right = n, mid;
     while(left <= right){
         mid = (left+right)/2;
         
@@ -28,10 +28,10 @@ int binarySearch(int val){
 }
 
 int main() {
-    scanf("%d", &n);
+    scanf("%d", &n); n+=2;
     update(1,1);
-    update(n+2,1);
-    for(int cnt=0; cnt<n; ++cnt){
+    update(n,1);
+    for(int cnt=2; cnt<n; ++cnt){
         scanf("%d", &v); ++v;
         int vSum = sumOneToNum(v), left = binarySearch(vSum-1), right = binarySearch(vSum);
         result += (distanceFromRoot[v] = max(distanceFromRoot[left], distanceFromRoot[right])+1);
