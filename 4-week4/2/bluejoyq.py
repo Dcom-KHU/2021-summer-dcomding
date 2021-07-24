b44f1318e13a7bc1d5c6172e38b2e5848046fcbc
bluejoyq <bluejoyq@naver.com>
2021-07-24 11:08:09 +0000

    4-2 ?

--- 4-week4/2/bluejoyq.py
+++ 4-week4/2/bluejoyq.py
@@ -1,16 +1,18 @@
-import sys 
-input = sys.stdin.readline 
+import sys
+
+input = sys.stdin.readline
+
 
 def solve():
     n = int(input())
     values = [0] * n
     for i in range(n):
         # end, start
-        s,e = map(int,input().split())
-        values[i] = [e,s]
-    values.sort() # end를 기준으로 정렬 
-    #print(values)
-    
+        s, e = map(int, input().split())
+        values[i] = [e, s]
+    values.sort()  # end를 기준으로 정렬
+    # print(values)
+
     cur = 0
     result = 0
     for end, start in values:
@@ -18,6 +20,8 @@ def solve():
             continue
         result += 1
         cur = end
-    
+
     print(result)
+
+
 solve()
