f5613eae03c719ea05bd583f16a5e7c2ce90429e
Jaehooni <8362532@gmail.com>
2021-07-24 11:42:41 +0000

    Add 4-week4/2/Jaehooni.py

--- 4-week4/2/Jaehooni.py
+++ 4-week4/2/Jaehooni.py
@@ -1,19 +1,16 @@
 n = int(input())
-times = [tuple(map(int, input().split())) for i in range(n)]
-timetable = dict([(i, True) for i in range(2 << 31)])
+times = sorted([tuple(map(int, input().split())) for i in range(n)], key=lambda x: x[0])
 
-def r(times, index, timetable):
+def r(index, start):
     if (index >= len(times)):
         return 0
-    
-    elif (timestable[times[index][0]+1] or timestable[times[index[index][1]-1]]):
-        return r(times, index+1, timetable)
-        
+
+    elif (start > times[index][0]):
+        return r(index+1, start)
+
     else:
-        timetable_copy = timetable_copy()
-        for i in range(times[index][0], times[index][1]+1):
-            timetable_copy[i] = False
-            
-        return max(1 + r(times, index+1, timetable_copy), r(times, index+1, timetable))
-    
-print(r(times,0,timetable))
+        new_start = times[index][1]
+        return max(r(index+1, start), 1+r(index+1,new_start))
+
+
+print(r(0, 0))
