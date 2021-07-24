fb7e8889e69783c8dc36a906955684e6f9808d04
Untaini <55027231+Untaini@users.noreply.github.com>
2021-07-24 22:52:57 +0900

    Add runtime

--- 4-week4/5/Untaini.cpp
+++ 4-week4/5/Untaini.cpp
@@ -1,4 +1,4 @@
-//solving with union-find, backtracking
+//solving with union-find, backtracking : 70ms
 #include <cstdio>
 #include <stack>
 #include <algorithm>
@@ -7,7 +7,8 @@ using namespace std;
 #define pii pair<int, int>
 #define max(a,b) (a>b?a:b)
 
-int n, v, left, right, leftGroup[100002], rightGroup[100002], disFromRoot[100002], res;
+int n, v, left, right, leftGroup[100002], rightGroup[100002], disFromRoot[100002];
+long long res;
 stack<int> vStack;
 stack<pair<int,pii>> rangeStack;
 
@@ -20,7 +21,6 @@ int findRight(int num){
     if(rightGroup[num] == num) return num;
     else return rightGroup[num] = findRight(rightGroup[num]);
 }
-
 
 int main()
 {
@@ -37,7 +37,7 @@ int main()
         v = vStack.top(); vStack.pop();
         left = findLeft(v-1), right = findRight(v+1);
         leftGroup[v] = left, rightGroup[v] = right;
-        //findLeft(right-1), findRight(left+1);
+        findLeft(right-1), findRight(left+1);
         rangeStack.push(make_pair(v,make_pair(left,right)));   
     }
     
@@ -45,44 +45,19 @@ int main()
         v = rangeStack.top().first, left = rangeStack.top().second.first, right = rangeStack.top().second.second;
         rangeStack.pop();
         res+=(disFromRoot[v] = max(disFromRoot[left], disFromRoot[right])+1)-1;
-        printf("%d\n",res);
+        printf("%lld\n",res);
     }
     return 0;
 }
 
 /*
-//solved with set container
-#include <cstdio>
-#include <set>
-using namespace std;
-
-#define max(x,y) (x>y?x:y)
-
-int n, v, distanceFromRoot[100002], result;
-set<int> insertedNum;
-
-int main() {
-    scanf("%d", &n);
-    insertedNum.insert(0);
-    insertedNum.insert(n+1);
-    for(int cnt=0; cnt<n; ++cnt){
-        scanf("%d", &v);
-        auto right = insertedNum.lower_bound(v), left = right; --left;
-        result += (distanceFromRoot[v] = max(distanceFromRoot[*left], distanceFromRoot[*right])+1);
-        insertedNum.insert(v);
-        printf("%d\n", --result);
-    }
-    return 0;
-}
-*/
-
-/*
-//solved with fenwick tree and binary search
+//solved with fenwick tree and binary search : 200ms
 #include <cstdio>
 
 #define max(x,y) (x>y?x:y)
 
-int n, v, fenwickTree[100010], distanceFromRoot[100010], result;
+int n, v, fenwickTree[100010], distanceFromRoot[100010];
+long long result;
 
 void update(int num, int val){
     while(num<=n)
@@ -101,22 +76,49 @@ int binarySearch(int val){
     while(left <= right){
         mid = (left+right)/2;
         
-        if(sumOneToNum(mid) <= val) left = mid+1;
+        if(sumOneToNum(mid <= val) left = mid+1;
         else right = mid-1;
     }
     return left;
 }
 
 int main() {
-    scanf("%d", &n); n+=2;
+    ("%d", &n); n+=2;
     update(1,1);
     update(n,1);
     for(int cnt=2; cnt<n; ++cnt){
         scanf("%d", &v); ++v;
         int vSum = sumOneToNum(v), left = binarySearch(vSum-1), right = binarySearch(vSum);
-        result += (distanceFromRoot[v] = max(distanceFromRoot[left], distanceFromRoot[right])+1);
+        result += (distanceFromRoot[v] = max(distanceFromRoot[left], distanceFromRoot[right])+1)-1;
         update(v, 1);
-        printf("%d\n", --result);
+        printf("%lld\n", result);
+    }
+    return 0;
+}
+*/
+
+/*
+//solved with set container : 300ms
+#include <cstdio>
+#include <set>
+using namespace std;
+
+#define max(x,y) (x>y?x:y)
+
+int n, v, distanceFromRoot[100002];
+lojg long result;
+set<int> insertedNum;
+
+int main() {
+    scanf("%d", &n);
+    insertedNum.insert(0);
+    insertedNum.insert(n+1);
+    for(int cnt=0; cnt<n; ++cnt){
+        scanf("%d", &v);
+        auto right = insertedNum.lower_bound(v), left = right; --left;
+        result += (distanceFromRoot[v] = max(distanceFromRoot[*left], distanceFromRoot[*right])+1)-1;
+        insertedNum.insert(v);
+        printf("%lld\n", result);
     }
     return 0;
 }
