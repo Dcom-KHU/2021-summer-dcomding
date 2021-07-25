210a3c2142cff5877cfcbba7c3ce3261fd495956
bluejoyq <bluejoyq@naver.com>
2021-07-25 07:31:56 +0000

    4-5 change to union-find

--- 4-week4/5/bluejoyq.py
+++ 4-week4/5/bluejoyq.py
@@ -1,37 +1,63 @@
-  
-n = int(input())
-vList = list(map(int, input().split()))
-rangeList =[0]*n
-leftGroup = [0]*100002
-rightGroup = [0]*100002
-disFromRoot = [0]*100002
+import sys
 
-def findLeft(num):
-    while leftGroup[num] != num:
-        num = leftGroup[num]
-    return num
-		
-def findRight(num):
-    while rightGroup[num] != num:
-        num = rightGroup[num]
-    return num
-		
-for cnt in range(100002):
-	leftGroup[cnt] = rightGroup[cnt] = cnt
-	
-for cnt in range(n-1,-1,-1):
-	v = vList[cnt]
-	left, right = findLeft(v-1), findRight(v+1)
-	leftGroup[v], rightGroup[v] = left, right
-	rangeList[cnt] = [left, right]
-	findLeft(right-1)
-	findRight(left+1)
+def solve():
+    N = 10000
+    values = [i + 1 for  i in range(N)]
+    
+    #N = 3
+    #values = [3,1,2]
 
-res=0
-result = ""
-for cnt in range(n):
-	v, left, right = vList[cnt], rangeList[cnt][0], rangeList[cnt][1]
-	disFromRoot[v] = max(disFromRoot[left], disFromRoot[right])+1
-	res += disFromRoot[v]-1
-	result += str(res) + "\n"
-print(result)
+    result = ""
+    num = 0
+    lo_max = [0] * (N + 2)
+    hi_min = [0]* (N + 2)
+    depth = [-1] * (N+1)
+    for i in range(1,N+1):
+        lo_max[i] = i - 1
+        hi_min[i] = i + 1
+    hi_min[N+1] = N+1
+    
+    def find_lo_max(cur,cur_stk):
+        while lo_max[cur] != cur:
+            cur_stk.append(cur)
+            cur = lo_max[cur]
+            
+        return cur
+    
+    def find_hi_min(cur,cur_stk):
+        while hi_min[cur] != cur:
+            cur_stk.append(cur)
+            cur = hi_min[cur]
+            
+        return cur
+    for value in values:
+        #print(lo_max)
+        #print(hi_min)
+        #print(value)
+        lo_stk = []
+        lo = find_lo_max(value, lo_stk)
+        
+        hi_stk = []
+        hi = find_hi_min(value, hi_stk)
+            
+        if 0 < lo < value:
+            cur = lo
+        elif value < hi < N+1:
+            cur = hi
+        else:
+            cur = value
+        #print(value, cur, lo, hi, lo_stk,hi_stk)
+        depth[value] = depth[cur] + 1
+        for l in lo_stk:
+            lo_max[l] = lo
+        for h in hi_stk:
+            hi_min[h] = hi
+        num += depth[value]
+        result += str(num) + "\n"
+    #print(lo_max)
+    #print(hi_min)
+    #print(depth)
+    print(result)
+solve()
+# 3 5 1 6 8 7 2 4
+        
