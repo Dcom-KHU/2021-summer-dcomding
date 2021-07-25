4c430fb91dcea35a1aa4ffbc85c1c379e8150041
Heeyeon Koo <rndus0819@naver.com>
2021-07-25 19:29:51 +0900

    첫번째 문제 solve

---
+++ 4-week4/1/heeyeonkoo99.py
@@ -0,0 +1,25 @@
+n,k=map(int,input().split())
+
+moves=list(map(int,input().split()))
+# print(moves)-체크용
+board=[0 for _ in range(n)]
+for i in range(n):
+    board[i]=list(map(int,input().split()))
+# print(board)-체크용
+
+def solve():
+    stack=[]
+    count=0
+    for m in moves:
+        for i in range(len(board)):
+            if board[i][m-1]!=0:
+                if stack and stack[-1]==board[i][m-1]:
+                    stack.pop()
+                    count+=2
+                else:
+                    stack.append(board[i][m-1])
+                board[i][m-1]=0 # 제대로 없애주기!
+                break
+    return count
+
+solve()
