7fadf0c3991adea7856081e1c30ce52af66fa3aa
Jangmanbo <wkddpdnjs99@gmail.com>
2021-07-25 19:39:29 +0900

    4-1

---
+++ 4-week4/1/Jangmanbo.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <queue>
+#include <sstream>
+using namespace std;
+
+int main() {
+	int n, k, num;
+	string str;
+	queue<int> moves;
+	vector<queue<int>> board(6);
+	stack<int> stk;
+
+	cin >> n >> k;
+
+	for (int i = 0; i < k; i++)
+	{
+		cin >> num;
+		moves.push(num);
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> num;
+			if (num) board[j].push(num);
+		}
+	}
+
+	int top, total = 0;
+	while (!moves.empty()) {
+		num = moves.front();
+		moves.pop();
+		if (!board[num].empty()) {
+			if (stk.empty()) top = 0;
+			else top = stk.top();
+
+			if (top == board[num].front()) {
+				stk.pop();
+				total += 2;
+			}
+			else {
+				stk.push(board[num].front());
+			}
+			board[num].pop();
+		}
+	}
+	cout << total;
+	return 0;
+}
