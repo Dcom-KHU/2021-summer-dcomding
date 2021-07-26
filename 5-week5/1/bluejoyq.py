aa2dde3418fb996c7fc53047e380f06ccb9aea01
bluejoyq <bluejoyq@naver.com>
2021-07-26 05:44:26 +0000

    5-1 final solve

--- 5-week5/1/bluejoyq.py
+++ 5-week5/1/bluejoyq.py
@@ -1,15 +1,16 @@
 def solve():
     s = input()
+    lst = s[0]
+    result = 1
+    for cur in s[1:]:
+        if lst != cur:
+            lst = cur
+            result +=1
+    print(result // 2)
     # 0으로 이루어진 블록과 1로 이루어진 블록의 개수를 센다
     # 둘 중 작은 것을 리턴
     # O(N)으로 가능. 더 줄이는 방법은?
-    result ={"0":0, "1":0}
-    lst = s[0]
-    result[lst]= 1
-    for cur in s[1:]:
-        if lst == cur:
-            continue
-        lst = cur
-        result[lst] += 1
-    print(min(result.values()))
+    
+    # 블록의 개수 / 2 하면 된다. 어차피 둘은 차이나봐야 최대 1 차이나므로
+    # 최솟값을 찾으려면 // 연산으로 나머지를 버리면 된다.
 solve()
