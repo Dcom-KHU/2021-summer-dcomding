e7b829c2b4751faa5cb13789937747d594073cfc
Untaini <55027231+Untaini@users.noreply.github.com>
2021-07-27 22:02:50 +0900

    Upload 5-week5/3 solving code

---
+++ 5-week5/3/Untaini.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+#define pii pair<int,int>
+
+int n, st, ed;
+vector<pii> timetable;
+vector<int> computers;
+priority_queue<pii> timeQueue;
+
+int main()
+{
+    scanf("%d",&n);
+    for(int cnt=0; cnt<n; ++cnt){
+        scanf("%d%d",&st,&ed);
+        timetable.push_back(make_pair(st,ed));
+    }
+    
+    sort(timetable.begin(), timetable.end());
+    computers.push_back(0);
+    timeQueue.push(make_pair(0,0));
+    
+    for(auto iter = timetable.begin(); iter != timetable.end(); ++iter){
+        pii time = timeQueue.top();
+
+        if(-iter->first>time.first){
+            computers.push_back(1);
+            timeQueue.push(make_pair(-iter->second,computers.size()-1));
+        }
+        else{
+            timeQueue.pop();
+            ++computers[time.second];
+            timeQueue.push(make_pair(-iter->second,time.second));
+        }
+    }
+    
+    for(auto iter = computers.begin(); iter != computers.end(); ++iter)
+        printf("%d ", *iter);
+    
+    return 0;
+}
