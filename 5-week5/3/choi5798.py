c1eafedaf918bc6bb503c63c61c774a60a935d2e
choi5798 <vlflalektzja@naver.com>
2021-07-29 19:44:56 +0900

    5-3 2t

--- 5-week5/3/choi5798.py
+++ 5-week5/3/choi5798.py
@@ -5,34 +5,34 @@ import heapq
 from collections import deque
 input = sys.stdin.readline
 n = int(input())
-timetable = []
+timetable = {}
 answer = 0
 count = [0 for _ in range(n)]
 index = [i for i in range(n)]
 using_list = []
 for _ in range(n):
     start, end = map(int, input().split())
-    timetable.append([start, end])
+    if start not in timetable:
+        timetable[start] = [end]
+    else:
+        timetable[start].append(end)
 
-timetable.sort(key=lambda x:(x[0], x[1]))
-timetable = deque(timetable)
+for key in timetable.keys():
+    timetable[key] = deque(sorted(timetable[key]))
 heapq.heapify(index)
 
-time = 0
-while timetable:
-    
-    if using_list and using_list[0][0] == time: # 끝내기
-        end, start, idx = heapq.heappop(using_list)
-        heapq.heappush(index, idx)
-    
-    if timetable[0][0] == time: # 시작하기
-        start, end = timetable.popleft()
+for key in sorted(timetable.keys()):
+    while timetable[key]:
+        if using_list and using_list[0][0] <= key: # 끝내기
+            end, start, idx = heapq.heappop(using_list)
+            heapq.heappush(index, idx)
+        
+         # 시작하기
+        start = key
+        end = timetable[key].popleft()
         idx = heapq.heappop(index)
         heapq.heappush(using_list, [end, start, idx]) # end 순으로 정렬
         count[idx] += 1
-        continue
-    
-    time += 1
 
 for i in count:
     if i == 0:
