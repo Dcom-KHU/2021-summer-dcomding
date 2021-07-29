5286df8cd11686d568cf7a01cc2f3e77715de81a
choi5798 <vlflalektzja@naver.com>
2021-07-29 20:28:00 +0900

    asdf

--- 5-week5/3/choi5798.py
+++ 5-week5/3/choi5798.py
@@ -1,45 +1,41 @@
-#총 컴퓨터 갯수 answer
-#각 컴퓨터당 사용 횟수 count
 import sys
-import heapq
 from collections import deque
+import heapq
 input = sys.stdin.readline
+
 n = int(input())
-timetable = {}
-answer = 0
+timetable = []
+using_list = []
 count = [0 for _ in range(n)]
 index = [i for i in range(n)]
-using_list = []
+answer = 0
 for _ in range(n):
     start, end = map(int, input().split())
-    if start not in timetable:
-        timetable[start] = [end]
-    else:
-        timetable[start].append(end)
+    timetable.append([start, end])
 
-for key in timetable.keys():
-    timetable[key] = deque(sorted(timetable[key]))
+timetable.sort(key=lambda x:(x[0], x[1]))
+timetable = deque(timetable)
 heapq.heapify(index)
 
-for key in sorted(timetable.keys()):
-    while timetable[key]:
-        if using_list and using_list[0][0] <= key: # 끝내기
-            end, start, idx = heapq.heappop(using_list)
-            heapq.heappush(index, idx)
-        
-         # 시작하기
-        start = key
-        end = timetable[key].popleft()
+time = 0
+while timetable:
+    if using_list and using_list[0][0] == time:
+        end, start, idx = heapq.heappop(using_list)
+        heapq.heappush(index, idx)
+
+    if timetable[0][0] == time:
+        start, end = timetable.popleft()
         idx = heapq.heappop(index)
-        heapq.heappush(using_list, [end, start, idx]) # end 순으로 정렬
+        heapq.heappush(using_list, [end, start, idx])
         count[idx] += 1
+        continue
+    
+    time += 1
+
 
 for i in count:
     if i == 0:
         break
     answer += 1
-
 print(answer)
 print(*count[:answer])
-    
-
