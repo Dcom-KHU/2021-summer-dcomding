1545b4d71fa58fb4d274adb74b2f52530d531783
KMileStone <tdj03204@naver.com>
2021-08-01 22:49:13 +0900

    submit 5-week5/5

--- 5-week5/5/KMileStone.py
+++ 5-week5/5/KMileStone.py
@@ -4,43 +4,30 @@ for i in range(n):
     words.append(input())
 
 
-# tree
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.child = []
-
-
 # tree : head - h - e - l - l - o - '\0'
 #                             - '\0'
 #                     - a - v - e - n - '\0'
 #             - g - o - o - d - b - y - e - '\0'
-head = Node('head')
+head = {}
 cursor = head
-found = False
 
 # make dictionary
 for word in words:
     cursor = head
 
     for c in word+'\0':
-        # find char from child
-        for node in cursor.child:
-            if node.val == c:
-                cursor = node
-                found = True
-                break
+        node = cursor.get(c)
 
         # if not found, make new node
-        if not found:
-            node = Node(c)
-            cursor.child.append(node)
-            cursor = node
-        else:
-            found = False
+        if not node:
+            node = {}
+            cursor[c] = node
+
+        cursor = node
+
 
 # count input char
-if len(head.child) == 1:
+if len(head) == 1:
     count = [1 for i in range(n)]
 else:
     count = [0 for i in range(n)]
@@ -50,18 +37,10 @@ for i in range(n):
 
     for c in words[i]:
         # if more than 1 child exist, you should choose char
-        if len(cursor.child) > 1:
+        if len(cursor) > 1:
             count[i] += 1
 
-            # find char from child
-            for node in cursor.child:
-                if node.val == c:
-                    cursor = node
-                    break
-
-        # else, auto
-        else:
-            cursor = cursor.child[0]
+        cursor = cursor.get(c)
 
 
 for e in count:
