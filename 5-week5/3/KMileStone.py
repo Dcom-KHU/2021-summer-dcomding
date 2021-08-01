856349838b0248a2ec67960ec15367dd18f87902
KMileStone <tdj03204@naver.com>
2021-08-01 21:15:26 +0900

    submit 5-week5/3

--- 5-week5/3/KMileStone.py
+++ 5-week5/3/KMileStone.py
@@ -11,20 +11,26 @@ timetable.sort(key=lambda x: x[0]*1000000 + x[1])
 
 computer = []
 
-# heap : [(end time, index of computer), (e, i), (e, i), ...]
-heap = []
+# heap_occupied : [(end time, index of computer), (e, i), (e, i), ...]
+# heap_available : [index of computer, i, i, ...]
+heap_occupied = []
+heap_available = []
 
 for user in timetable:
+    # update available
+    while heap_occupied and heap_occupied[0][0] <= user[0]:
+        heapq.heappush(heap_available, heapq.heappop(heap_occupied)[1])
+
     # use existing computer
-    if heap and heap[0][0] <= user[0]:
-        idx = heapq.heappop(heap)[1]
+    if heap_available:
+        idx = heapq.heappop(heap_available)
         computer[idx] += 1
-        heapq.heappush(heap, (user[1], idx))
+        heapq.heappush(heap_occupied, (user[1], idx))
 
     # use new computer
     else:
         computer.append(1)
-        heapq.heappush(heap, (user[1], len(computer)-1))
+        heapq.heappush(heap_occupied, (user[1], len(computer)-1))
 
 
 print(len(computer))
