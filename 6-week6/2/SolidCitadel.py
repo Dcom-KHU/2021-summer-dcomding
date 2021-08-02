81fdf51bca4ae81d2064b54bef38048fa49535dd
SolidCitadel <teddy3582@naver.com>
2021-08-03 00:33:42 +0900

    Create SolidCitadel.py

---
+++ 6-week6/2/SolidCitadel.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+count = 0
+i = 0
+for an in A:
+    while i < b and B[i] <= an:
+        if B[i] == an:
+            count += 1
+        i += 1
+        
+print(a + b - 2*count)
