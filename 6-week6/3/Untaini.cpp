0a228c9dad66125b4bbb5361d3e4fb7f48628006
Untaini <55027231+Untaini@users.noreply.github.com>
2021-08-02 23:02:54 +0900

    방문 확인용 변수 course[x][y] -> visited[x][y] 로 변경

--- 6-week6/3/Untaini.cpp
+++ 6-week6/3/Untaini.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define pii pair<int, int>
 
 int n,m, map[501][501], course[501][501], dx[]={1,-1,0,0}, dy[]={0,0,1,-1};
+bool visited[501][501];
 stack<pair<int, pii>> funcStack;
 
 bool canMove(int x, int y){
@@ -20,14 +21,14 @@ int main()
             scanf("%d",&map[nCnt][mCnt]);
             
     funcStack.push(make_pair(0,make_pair(0,0)));
-    course[n-1][m-1] = 1;
+    course[n-1][m-1] = visited[n-1][m-1] = 1;
     
     while(!funcStack.empty()){
         pii pos = funcStack.top().second;
         int type = funcStack.top().first, x = pos.first, y = pos.second;
         funcStack.pop();
         
-        if(course[x][y]) continue;
+        if(visited[x][y]) continue;
         
         if(!type){
             funcStack.push(make_pair(1,pos));
@@ -43,6 +44,7 @@ int main()
                 if(canMove(nx,ny) && map[x][y] > map[nx][ny])
                     course[x][y] += course[nx][ny];
             }
+            visited[x][y] = 1;
         }
     }
     
