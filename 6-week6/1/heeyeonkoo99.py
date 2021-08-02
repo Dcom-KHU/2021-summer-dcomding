eb60f2374dc5d00f251b5f2137fd382debc4894d
Heeyeon Koo <rndus0819@naver.com>
2021-08-02 12:02:14 +0900

    수정

--- 6-week6/1/heeyeonkoo99.py
+++ 6-week6/1/heeyeonkoo99.py
@@ -3,11 +3,13 @@ arr=[]
 for i in range(n):
     arr.append(i+1)
 
-while len(arr)!=1:
+while True:
+    if len(arr)==1:
+        print(arr[0])
+        return
     temp=[]
     for i in range(1,len(arr)+1):
         if i%2==0:
             temp.append(arr[i-1])
     arr=temp
     
-print(arr[0])
