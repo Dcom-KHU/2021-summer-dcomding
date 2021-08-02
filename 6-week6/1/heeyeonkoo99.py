3cd3f07598fcc9c8dd9b8ccd15a6ea0fc33a5d1b
Heeyeon Koo <rndus0819@naver.com>
2021-08-02 11:57:38 +0900

    week6-1

---
+++ 6-week6/1/heeyeonkoo99.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(i+1)
+
+while len(arr)!=1:
+    temp=[]
+    for i in range(1,len(arr)+1):
+        if i%2==0:
+            temp.append(arr[i-1])
+        else:
+            continue
+    arr=temp
+print(arr[0])
