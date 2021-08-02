02ad5cf0a424452cf07843443ecf018b8369ce4a
Untaini <55027231+Untaini@users.noreply.github.com>
2021-08-02 23:08:27 +0900

    Delete variable visited[][]

--- 6-week6/3/Untaini.cpp
+++ 6-week6/3/Untaini.cpp
@@ -6,7 +6,6 @@ using namespace std;
 #define pii pair<int, int>
 
 int n,m, map[501][501], course[501][501], dx[]={1,-1,0,0}, dy[]={0,0,1,-1};
-bool visited[501][501];
 stack<pair<int, pii>> funcStack;
 
 bool canMove(int x, int y){
@@ -17,34 +16,36 @@ int main()
 {
     scanf("%d%d",&n,&m);
     for(int nCnt=0; nCnt<n; ++nCnt)
-        for(int mCnt=0; mCnt<m; ++mCnt)
+        for(int mCnt=0; mCnt<m; ++mCnt){
             scanf("%d",&map[nCnt][mCnt]);
+            course[nCnt][mCnt] = -1;
+        }
             
     funcStack.push(make_pair(0,make_pair(0,0)));
-    course[n-1][m-1] = visited[n-1][m-1] = 1;
+    course[n-1][m-1] = 1;
     
     while(!funcStack.empty()){
         pii pos = funcStack.top().second;
         int type = funcStack.top().first, x = pos.first, y = pos.second;
         funcStack.pop();
         
-        if(visited[x][y]) continue;
+        if(course[x][y] != -1) continue;
         
         if(!type){
             funcStack.push(make_pair(1,pos));
             for(int cnt=0; cnt<4; ++cnt){
                 int nx = x+dx[cnt], ny = y+dy[cnt];
-                if(canMove(nx,ny) && !course[nx][ny] && map[x][y] > map[nx][ny])
+                if(canMove(nx,ny) && course[nx][ny]==-1 && map[x][y] > map[nx][ny])
                     funcStack.push(make_pair(0,make_pair(nx,ny)));
             }
         }
         else{
+            course[x][y]=0;
             for(int cnt=0; cnt<4; ++cnt){
                 int nx = x+dx[cnt], ny = y+dy[cnt];
                 if(canMove(nx,ny) && map[x][y] > map[nx][ny])
                     course[x][y] += course[nx][ny];
             }
-            visited[x][y] = 1;
         }
     }
     
