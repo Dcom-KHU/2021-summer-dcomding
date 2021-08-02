dd7a317d06a5f7235141f7f6a20a01ba9d6357b8
SolidCitadel <teddy3582@naver.com>
2021-08-03 00:35:12 +0900

    Update SolidCitadel.py

--- 6-week6/2/SolidCitadel.py
+++ 6-week6/2/SolidCitadel.py
@@ -1,13 +1,16 @@
+from collections import defaultdict
+
 a, b = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+dc = defaultdict(int)
+for n in A+B:
+    dc[n] += 1
+
 count = 0
-i = 0
-for an in A:
-    while i < b and B[i] <= an:
-        if B[i] == an:
-            count += 1
-        i += 1
-        
-print(a + b - 2*count)
+for v in dc.values():
+    if v == 1:
+        count += 1
+
+print(count)
