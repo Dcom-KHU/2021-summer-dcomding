1d8ae4c3ff2ecc44b5d13203c3b31d103a02ded3
choi5798 <vlflalektzja@naver.com>
2021-08-02 16:38:29 +0900

    6-1 3t

--- 6-week6/1/choi5798.cpp
+++ 6-week6/1/choi5798.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    unsigned long long n;
-    int answer = 0;
-    int i=1;
-    cin >> n;
-    for(; i<=n; i*=2)
+    while (1)
     {
-        if(i > n) break;
+        unsigned long long n;
+        int answer = 0;
+        int i=1;
+        cin >> n;
+        // for(; i<=n; i*=2)
+        // {
+        //     if(i > n) break;
+        // }
+        int log_val = log2l(n);
+        cout << (unsigned long long)pow(2, log_val) << "\n";
     }
-    cout << (unsigned long long)i/2;
+    
+    
     
 
     
