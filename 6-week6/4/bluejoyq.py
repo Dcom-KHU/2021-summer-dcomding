5ea3559bf2b32d64b86f44a3a33908d29856b622
bluejoyq <bluejoyq@naver.com>
2021-08-03 12:16:00 +0000

    6-4 solve

--- 6-week6/4/bluejoyq.py
+++ 6-week6/4/bluejoyq.py
@@ -1,8 +1,35 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(200000)
 def solution():
     N = int(input())
     values = list(map(int, input().split()))
+    edges = {i:[] for i in range(N)}
     for i in range(N-1):
-        input()
-    print(sum(values))
+        a,b = map(int, input().split())
+        edges[a].append(b)
+        edges[b].append(a)
+    if sum(values):
+        return(-1)
+    # 방향성이 없는 그래프이므로 간선이 1개인 node 아무거나 찾아서 root로 취급.
+    for root in range(N):
+        if len(edges[root]) == 1:
+            break
+    global result
+    result = 0
+    visited = [0 for i in range(N)]
+    visited[root] = 1
+    stk = [] # 재귀 대신
     
-solution()
+    def recur_make_zero(cur, visited, values,edges):
+        global result
+        for nxt in edges[cur]:
+            if visited[nxt]:
+                continue
+            visited[nxt] = 1
+            recur_make_zero(nxt, visited, values,edges)
+            values[cur] += values[nxt]
+            result += abs(values[nxt])
+    recur_make_zero(root, visited, values,edges)
+    return result
+print(solution())
