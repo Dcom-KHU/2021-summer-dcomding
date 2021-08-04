18e7d69fd41533c504ea268b9915a91b25008cb6
ZION <sju0924@khu.ac.kr>
2021-08-04 21:00:18 +0900

    Update sju0924.cpp

--- 6-week6/4/sju0924.cpp
+++ 6-week6/4/sju0924.cpp
@@ -1,14 +1,11 @@
 #include <string>
 #include <vector>
 #include<iostream>
-
 using namespace std;
-
 vector<long long int> node;
 vector<vector<int>> edge;
 vector<int> visit;
 long long int totalCount = 0;
-
 
 long long int Abs(long long int n) {
 	if (n > 0) return n;
@@ -30,17 +27,9 @@ long long int searchCount(int n) {
 
 	return node[n];
 }
-long long solution(vector<int> a, vector<vector<int>> edges) {
+long long solution() {
 	long long answer = -2;
-	for (int i = 0; i < a.size();i++) {
-		node.push_back(a[i]);
-	}
-	edge = vector<vector<int>>(a.size());
-	visit = vector<int>(a.size());
-	for (int i = 0; i < edges.size();i++) {
-		edge[edges[i][0]].push_back(edges[i][1]);
-		edge[edges[i][1]].push_back(edges[i][0]);
-	}
+
 
 	int res;
 	res = searchCount(0);
@@ -50,19 +39,25 @@ long long solution(vector<int> a, vector<vector<int>> edges) {
 
 	return answer;
 }
-int main() {
-	int p,c,n;
-	cin >> n;
 
-	vector<vector<int>> ed(0);
-	vector<int>a(n);
+int main() {
+	int p,c,n,input;
+	cin >> n;
+	
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		cin >>input;
+		node.push_back(input);
 	}
+	visit = vector<int>(n);
+	edge = vector<vector<int>>(n);
 	for (int i = 0; i < n-1; i++) {
 		cin >> p>>c;
-		ed.push_back({ p,c });
+		edge[p].push_back(c);
+		edge[c].push_back(p);
 	}
 
-	cout<<solution(a, ed);
+	long long int res = solution();
+	cout << res<<"\n";
+
+	return 0;
 }
