c6f9d404c31be8e6ae8e9bc8ac67e0d0885c5c93
ZION <sju0924@khu.ac.kr>
2021-08-04 23:57:04 +0900

    Update sju0924.cpp

--- 6-week6/4/sju0924.cpp
+++ 6-week6/4/sju0924.cpp
@@ -1,11 +1,34 @@
 #include <string>
 #include <vector>
 #include<iostream>
+#include<stack>
 using namespace std;
 vector<long long int> node;
 vector<vector<int>> edge;
 vector<int> visit;
+
+stack<int> nt;
 long long int totalCount = 0;
+
+struct Node {
+	int idx;
+	int childidx;
+
+	Node(int i, int c) {
+		idx = i;
+		childidx = c;
+	}
+	Node() {
+		idx = -1;
+		childidx = -1;
+	}
+	void setNode(int i, int c) {
+		idx = i;
+		childidx = c;
+	}
+};
+
+stack<Node> st;
 
 long long int Abs(long long int n) {
 	if (n > 0) return n;
@@ -27,12 +50,50 @@ long long int searchCount(int n) {
 
 	return node[n];
 }
+void PushStack(int n, char kind) {
+	Node newnode;
+	if (kind == 'n') {
+		visit[n] = 1;
+		for (int i = 0; i < edge[n].size();i++) {
+			if (!visit[edge[n][i]]) {
+				nt.push(edge[n][i]);
+				newnode.setNode(n, edge[n][i]);
+				st.push(newnode);
+			}
+			
+		}
+	}
+
+}
 long long int solution() {
 	long long int answer = -2;
 	long long int res;
 
-	res = searchCount(0);
+	int t;
+	Node Top;
+	long long int sub = 0;
+	nt.push(0);
+	while (!nt.empty()) {
+		t = nt.top();
+		nt.pop();
+		PushStack(t, 'n');
+	}
 
+	
+	while (!st.empty()) {
+		Top = st.top();
+		st.pop();
+
+		//cout << Top.idx << " " << Top.childidx << "\n";
+		
+		node[Top.idx] += node[Top.childidx];		
+		totalCount += Abs(node[Top.childidx]);
+		node[Top.childidx] = 0;
+		//cout << totalCount << "\n";
+	}
+	//res = searchCount(0);
+
+	res = node[0];
 	if (!res) answer = totalCount;
 	else answer = -1;
 
