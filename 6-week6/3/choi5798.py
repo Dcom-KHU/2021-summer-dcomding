55bdafcd9da7cea86e100d3223227dcca7e02d18
choi5798 <vlflalektzja@naver.com>
2021-08-05 18:23:12 +0900

    6-3 unlimited

--- 6-week6/3/choi5798.py
+++ 6-week6/3/choi5798.py
@@ -1,4 +1,6 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(20000)
+input = sys.stdin.readline
 n, m = map(int, input().split())
 map_val = []
 for _ in range(n):
@@ -6,35 +8,32 @@ for _ in range(n):
     map_val.append(temp)
 
 dxy = [(-1,0),(1,0),(0,-1),(0,1)] # 상, 하, 좌, 우
-route = deque()
+dp = [[-1 for _ in range(m)] for _ in range(n)]
+#dp[y][x]: 점x,y에서 목적지(점m-1,n-1)까지 갈 수 있는 길의 수
 
-# def is_inside(y, x):
-#     return 0 <= y < n and 0 <= x < m
+def is_inside(y, x):
+    return 0 <= y < n and 0 <= x < m
 
-def get_new_route(y, x):
-    new_route = []
-    for dy, dx in dxy:
-        new_y = y+dy
-        new_x = x+dx
-        # if is_inside(new_y, new_x):
-        try:
-            if map_val[y][x] > map_val[new_y][new_x]: # 내리막길인지?
-                new_route.append([new_y, new_x])
-        except:
-            continue
-    return new_route
 
-def bfs(y, x):
-    answer = 0
-    route.append([y, x])
-    while route:
-        new_y, new_x = route.popleft()
-        if new_y == n-1 and new_x == m-1:
-            answer += 1
-            continue
-        new_route = get_new_route(new_y, new_x)
-        route.extend(new_route)
-        
-    return answer
+def dfs(y, x):
+    if y == n-1 and x == m-1:
+        return 1
+    
+    elif dp[y][x] == -1:
+        dp[y][x] = 0
+        for dy, dx in dxy:
+            new_y, new_x = y+dy, x+dx
+            if is_inside(new_y, new_x):
+                if map_val[y][x] > map_val[new_y][new_x]:
+                    # stack.append([new_y, new_x])
+                    dp[y][x] += dfs(new_y, new_x)
+    return dp[y][x]
+    
+#단순히 dfs로만 풀면 되는 문제인지 알고 호기롭게 덤볐다가
+# 삽질을 열심히 하던 중 시간초과의 배후가 있다는 사실을 알고
+#결국 dp가 필요하다는 도움을 받았고 생각보다 간단하게 풀렸다..
+#dfs와 dp의 조합이 너무 신선했다
 
-print(bfs(0, 0))
+
+
+print(dfs(0, 0))
