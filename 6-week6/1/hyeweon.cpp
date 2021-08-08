32d690c5edc68106a9ee30880defe6791a91c487
hyeweon <berry_na@khu.ac.kr>
2021-08-08 15:22:59 +0900

    Create hyeweon.cpp
    week6

---
+++ 6-week6/1/hyeweon.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int answer = 1;
+	for (int i = 0; n >= 2; i++) {
+		answer = answer * 2;
+		n = n / 2;
+	}
+
+	cout << answer << endl;
+}
