5d770b68091bbe557256166d8368c91ff613b291
KMileStone <tdj03204@naver.com>
2021-08-08 21:41:40 +0900

    submit 6-week6/3

--- 6-week6/3/KMileStone.py
+++ 6-week6/3/KMileStone.py
@@ -6,33 +6,39 @@ for i in range(n):
 
 d = [[0 for i in range(m)] for j in range(n)]
 
-def dfs(x, y):
-    # if arrived
-    if (x,y) == (n-1,m-1):
-        return 1
+# recursive to stack
+stack = [(0, 0, -1, -1)]
 
-    # if reached known route
+while stack:
+    (x, y, prev_x, prev_y) = stack.pop()
+
+    # if arrived
+    if (x, y) == (n-1, m-1):
+        d[prev_x][prev_y] += 1
+
+    # if reached known route or returning
     elif d[x][y] != 0:
-        return d[x][y]
+        d[prev_x][prev_y] += d[x][y]
 
     else:
+        # return point
+        stack.append((x, y, prev_x, prev_y))
+
         # up
-        if 0 <= x-1 and matrix[x-1][y] < matrix[x][y]:
-            d[x][y] += dfs(x-1, y)
+        if 0 <= x - 1 and matrix[x - 1][y] < matrix[x][y]:
+            stack.append((x-1, y, x, y))
 
         # down
-        if x+1 < n and matrix[x+1][y] < matrix[x][y]:
-            d[x][y] += dfs(x+1, y)
+        if x + 1 < n and matrix[x + 1][y] < matrix[x][y]:
+            stack.append((x+1, y, x, y))
 
         # left
-        if 0 <= y-1 and matrix[x][y-1] < matrix[x][y]:
-            d[x][y] += dfs(x, y-1)
+        if 0 <= y - 1 and matrix[x][y - 1] < matrix[x][y]:
+            stack.append((x, y-1, x, y))
 
         # right
-        if y+1 < m and matrix[x][y+1] < matrix[x][y]:
-            d[x][y] += dfs(x, y+1)
-
-        return d[x][y]
+        if y + 1 < m and matrix[x][y + 1] < matrix[x][y]:
+            stack.append((x, y+1, x, y))
 
 
-print(dfs(0,0))
+print(d[0][0])
