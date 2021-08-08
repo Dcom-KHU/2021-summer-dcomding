0ae4c856da68dd9f566ee6716dc5d575bbbffe8b
KMileStone <tdj03204@naver.com>
2021-08-08 21:51:00 +0900

    submit 6-week6/3

--- 6-week6/3/KMileStone.py
+++ 6-week6/3/KMileStone.py
@@ -11,28 +11,30 @@ d[n-1][m-1] = 1
 stack = [(0, 0, -1, -1)]
 
 while stack:
-    (x, y, prev_x, prev_y) = stack[-1]
+    (x, y, prev_x, prev_y) = stack.pop()
 
     # if reached known route or returning
     if d[x][y] != 0:
         d[prev_x][prev_y] += d[x][y]
-        stack.pop()
 
     else:
+        # return point
+        stack.append((x, y, prev_x, prev_y))
+
         # up
-        if 0 <= x - 1 and matrix[x-1][y] < matrix[x][y]:
+        if 0 <= x-1 and matrix[x-1][y] < matrix[x][y]:
             stack.append((x-1, y, x, y))
 
         # down
-        if x + 1 < n and matrix[x+1][y] < matrix[x][y]:
+        if x+1 < n and matrix[x+1][y] < matrix[x][y]:
             stack.append((x+1, y, x, y))
 
         # left
-        if 0 <= y - 1 and matrix[x][y-1] < matrix[x][y]:
+        if 0 <= y-1 and matrix[x][y-1] < matrix[x][y]:
             stack.append((x, y-1, x, y))
 
         # right
-        if y + 1 < m and matrix[x][y+1] < matrix[x][y]:
+        if y+1 < m and matrix[x][y+1] < matrix[x][y]:
             stack.append((x, y+1, x, y))
 
 
