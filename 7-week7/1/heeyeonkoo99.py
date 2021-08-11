095ca1536ff6b595222885ded1ab056acf92fc2c
Heeyeon Koo <rndus0819@naver.com>
2021-08-11 23:30:27 +0900

    젠가

--- 7-week7/1/heeyeonkoo99.py
+++ 7-week7/1/heeyeonkoo99.py
@@ -1,17 +1,18 @@
 n,L=map(int,input().split())
 boxes=list(map(int,input().split()))
-flag=True
 center=0
+flag=True
 
-for i in range(1,n):
-    center+=boxes[i-1]
-    if boxes[i-1]-L<center/i<boxes[i-1]+L:
+for i in range(n-1,0,-1): # 그림의 위치를 떠올리면 순서대로 A,B,C,D...등이 있기에 뒤에서부터 돌린다.
+    center+=boxes[i] 
+    if boxes[i-1]-L<center/n-i<boxes[i-1]+L: # 그 바로 아래에 있는 box에 대해 보는것이다.
         continue
     else:
         flag=False
-        print("0")
         break 
 if flag==True:
     print("1")
+else:
+    print("0")
 
 
