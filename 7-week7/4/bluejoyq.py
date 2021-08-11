1f2ca7040782fda61e796caeab405da311d138ad
bluejoyq <bluejoyq@naver.com>
2021-08-11 08:31:21 +0000

    7-4 try

--- 7-week7/4/bluejoyq.py
+++ 7-week7/4/bluejoyq.py
@@ -1,32 +1,51 @@
+import sys 
+input = sys.stdin.readline
+MAX_VAL = 120120
 def solve():
     N = int(input())
     target = input().rstrip() 
-    
-    root = {}
+    words_by_first_char = {}
+    words = [0] * N
     for i in range(N):
-        chars = list(input())
-        cur = root
-        for char in chars:
-            try:
-                cur[char]
-            except:
-                cur[char] = {}
-            cur = cur[char] 
-        
+        tmp = input().rstrip()
+        words[i] = tmp
+        try:
+            words_by_first_char[tmp[0]].append(i)
+        except:
+            words_by_first_char[tmp[0]] = [i]
     M = len(target)
-    i = 0
-    result = 0
-    print(root)
-    while i < M:
-        cur = root.get(target[i])
-        if cur == None:
-            return -1
-        while cur != None:
-            i += 1
+    
+    cache = [[MAX_VAL] * (N) for i in range(M)]
+    
+    for nxt in words_by_first_char[target[0]]:
+        cache[0][nxt] = [1,1]
+    for i in range(1, M):
+        cur_char = target[i]
+        no_home = MAX_VAL
+        for j in range(N):
+            if cache[i - 1][j] == MAX_VAL:
+                continue
+            cur_idx, score = cache[i - 1][j]
             try:
-                cur = cur.get(target[i])
+                if cur_char != words[j][cur_idx]:
+                    continue
+                cache[i][j] = [cur_idx + 1, score]
             except:
-                return result + 1
-        result += 1
-    return result        
+                no_home = min(no_home, score)
+        # 새로 단어를 찾아야하면.
+        if no_home != MAX_VAL:
+            for nxt in words_by_first_char[cur_char]:
+                if cache[i][nxt] == MAX_VAL or cache[i][nxt][1] > no_home + 1:
+                    cache[i][nxt] = [1,no_home + 1]
+                
+    result = MAX_VAL
+    for i in range(N):
+        lst = cache[M-1][i]
+        if lst == MAX_VAL or lst[0] != len(words[i]):
+            continue
+        result = min(result, lst[1])
+
+    if result == MAX_VAL:
+        return -1
+    return result
 print(solve())
