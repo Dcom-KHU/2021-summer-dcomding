eddf49acdbb121fbbf64ad4d18f5946c91578c21
choi5798 <vlflalektzja@naver.com>
2021-08-11 12:27:28 +0900

    7-1 re

--- 7-week7/1/choi5798.py
+++ 7-week7/1/choi5798.py
@@ -4,14 +4,14 @@ stable = True
 total = 0
 for i in range(1, n):
     total += blocks[i-1]
-    print('{}번 박스의 구간: {} ~ {}'.format(n-i, blocks[i]-m, blocks[i]+m))
-    print('~{}번 박스의 중심범위: {}'.format(n-i+1, total/i))
+    # print('{}번 박스의 구간: {} ~ {}'.format(n-i, blocks[i]-m, blocks[i]+m))
+    # print('~{}번 박스의 중심범위: {}'.format(n-i+1, total/i))
     if blocks[i]-m < total / i < blocks[i]+m:
         pass
     else:
         stable = False
         break
 if stable:
-    print('stable')
+    print(1)
 else:
-    print('unstable')
+    print(0)
