2fd3f681486e8e794399cfbcd5d3210965e38fdb
Heeyeon Koo <rndus0819@naver.com>
2021-08-11 23:35:27 +0900

    젠가

--- 7-week7/1/heeyeonkoo99.py
+++ 7-week7/1/heeyeonkoo99.py
@@ -5,7 +5,7 @@ flag=True
 
 for i in range(n-1,0,-1): # 그림의 위치를 떠올리면 순서대로 A,B,C,D...등이 있기에 뒤에서부터 돌린다.
     center+=boxes[i] 
-    if boxes[i-1]-L<center/n-i<boxes[i-1]+L: # 그 바로 아래에 있는 box에 대해 보는것이다.
+    if boxes[i-1]-L<center/(n-i)<boxes[i-1]+L: # 그 바로 아래에 있는 box에 대해 보는것이다.
         continue
     else:
         flag=False
@@ -14,5 +14,4 @@ if flag==True:
     print("1")
 else:
     print("0")
-
 
