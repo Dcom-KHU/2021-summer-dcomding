75f1c129dab8b636333ead22cdf9c90d9c3490a4
ZION <sju0924@khu.ac.kr>
2021-08-15 21:28:53 +0900

    Update sju0924.cpp

--- 7-week7/5/sju0924.cpp
+++ 7-week7/5/sju0924.cpp
@@ -3,6 +3,7 @@
 #include<map>
 #include<vector>
 #include<algorithm>
+#include<stack>
 
 using namespace std;
 
@@ -25,8 +26,83 @@ int main() {
 		cost[i][1] = -1;
 		cost[i][0] = -1;
 	}
+
+	stack<vector<int>> st,st2;
+	vector<int> inp = { 0,0 }; //리더, 참석 여부
+	vector<int> t;
+	int leader, is_attend;
 	int res = Min(getCost(0, 0),getCost(0,1));
-	cout << res;
+	st.push(inp);
+	inp[1] = 1;
+	st.push(inp);
+	while (!st.empty()) {
+		t = st.top();
+		leader = t[0];
+		is_attend = t[1];
+		st.pop();
+
+		for (int i = 0; i < teams[leader].size();i++) {
+			inp[0] = teams[leader][i];
+			inp[1] = 0;
+			st2.push(inp);
+
+			inp[1] = 0;
+			st2.push(inp);
+			
+		}
+
+	}
+		
+	
+	while (!st2.empty()) {
+		t = st2.top();
+		leader = t[0];
+		is_attend = t[1];
+		st2.pop();
+
+		int result = 0;
+		if (is_attend) {
+			for (int i = 0; i < teams[leader].size();i++) {
+				result += Min(cost[teams[leader][i]][0], cost[teams[leader][i]][1]);
+			}
+			result += sales[leader];
+		}
+		else if (!is_attend) {
+			int next_ide;
+			int next_ede;
+			int min_cost = INF;
+			int min_node = -1;
+			bool found = false;
+
+			for (int i = 0; i < teams[leader].size();i++) {
+				next_ide = cost[teams[leader][i]][1];
+				next_ede = cost[teams[leader][i]][0];
+				if (next_ide < next_ede) {
+					result += cost[teams[leader][i]][1];
+					found = 1;
+				}
+				else if (!found && next_ede <= next_ide) {
+					if (next_ide - next_ede < min_cost) {
+						min_cost = next_ide - next_ede;
+						min_node = teams[leader][i];
+					}
+				}
+			}
+			if (!found && min_node != -1) {
+				for (int i = 0; i < teams[leader].size();i++) {
+					if (teams[leader][i] == min_node) {
+						result += cost[teams[leader][i]][1];
+					}
+					else {
+						result += cost[teams[leader][i]][0];
+					}
+				}
+			}
+		}
+
+		cost[leader][is_attend] = result;
+	}
+	cout << Min(cost[0][0],cost[0][1]);
 
 
 
