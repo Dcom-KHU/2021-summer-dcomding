8a5daa336cc89d8ae07c5e5562bf0111bb13db2e
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-08-15 10:04:52 +0000

    Solving code using Trie and DP

--- 7-week7/4/Untaini.cpp
+++ 7-week7/4/Untaini.cpp
@@ -1,3 +1,73 @@
+//Trie + DP로 풀기
+#include <cstdio>
+#include <cstring>
+#define min(a,b) (a<b?a:b)
+
+class Trie{
+public:
+    Trie* abt[26];
+    bool isEnd; //끝난지점을 확인하기 위해 변수 추가
+	
+    Trie(){
+        memset(abt, 0, sizeof(abt));
+		isEnd=false;
+    }
+    
+    ~Trie(){
+        for(int cnt=0; cnt<26; ++cnt)
+            if(abt[cnt]) delete abt[cnt];
+    }
+    
+    Trie* insert(int ch){
+        if(!abt[ch-='a'])
+			abt[ch] = new Trie();
+        return abt[ch];
+    }
+};
+
+//wordDp[n] : n-1번째 문자열을 완성하는데까지 사용한 단어의 최소 개수
+int n, wordDp[20010], cnt, len, cur;
+char str[20010], word[6];
+Trie root;
+
+int main(){
+	scanf("%d",&n); getchar();
+	scanf("%s", str);
+	for(cnt=0; cnt<n; ++cnt){
+		scanf("%s",word);
+		Trie* trie = &root;
+		
+		//단어를 트라이 자료구조에 넣음
+		for(cur=0; word[cur]; ++cur)
+			trie = trie->insert(word[cur]);
+		
+		//단어의 마지막 알파벳 위치에 단어가 끝났다는 것을 저장함
+		trie->isEnd = true;
+	}
+	
+	//DP 초기값 설정
+	for(cnt=1; cnt<=20000; ++cnt)
+		wordDp[cnt] = 1<<30;
+	
+	for(len=0; str[len]; ++len){
+		Trie* trie = &root; cur=len;
+		
+		//만약 트라이에 str[cur]에 해당하는 알파벳이 있다면 반복문 시작
+		while(trie->abt[str[cur]-'a']){
+			trie = trie->abt[str[cur++]-'a'];
+			
+			//단어가 끝났다면 DP값을 갱신함
+			if(trie->isEnd) wordDp[cur] = min(wordDp[cur], wordDp[len]+1);
+		}
+	}
+	
+	//주어진 단어들로 문자열을 만들 수 없다면 -1, 아니면 dp값을 출력함
+	printf("%d", wordDp[len]==(1<<30)?-1:wordDp[len]);
+	return 0;
+}
+
+/*
+//첫 알파벳으로 정리한 배열 + DP로 풀기
 #include <cstdio>
 #include <vector>
 #include <algorithm>
@@ -40,3 +110,4 @@ int main() {
 	printf("%d", wordDp[len]==(1<<30)?-1:wordDp[len]);
 	return 0;
 }
+*/
