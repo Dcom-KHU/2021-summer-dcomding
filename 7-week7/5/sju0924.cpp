84220be09adecce92ac91bcd6a52fd6c202149a0
ZION <sju0924@khu.ac.kr>
2021-08-15 21:31:21 +0900

    Update sju0924.cpp

--- 7-week7/5/sju0924.cpp
+++ 7-week7/5/sju0924.cpp
@@ -13,7 +13,6 @@ vector<vector<int>> links;
 vector<vector<int>> teams(300001,vector<int>(0));
 const int INF = 2147483647;
 
-int getCost(int leader, int is_attend);
 int setInfo();
 int Min(int a, int b);
 int main() {
@@ -31,7 +30,6 @@ int main() {
 	vector<int> inp = { 0,0 }; //리더, 참석 여부
 	vector<int> t;
 	int leader, is_attend;
-	int res = Min(getCost(0, 0),getCost(0,1));
 	st.push(inp);
 	inp[1] = 1;
 	st.push(inp);
@@ -127,57 +125,4 @@ int setInfo() {
 	}
 
 	return 0;
-}
-
-int getCost(int leader, int is_attend){ //is_attend는 leader가 참여하는지 여부
-	//cout << "cost[" << leader << "][" << is_attend << "] 구하는중 : ";
-	int result = 0;
-	int cur_result;
-	if (cost[leader][is_attend] != -1) {
-		return cost[leader][is_attend];
-	}
-	if (is_attend) {
-		for (int i = 0; i < teams[leader].size();i++) {
-			result += Min(getCost(teams[leader][i], 0), getCost(teams[leader][i], 1));
-		}
-		result += sales[leader];
-	}
-	else if (!is_attend) {
-		int next_ide;
-		int next_ede;
-		int min_cost=INF;
-		int min_node = -1;
-		bool found = false;
-
-		for (int i = 0; i < teams[leader].size();i++) {
-			next_ide = getCost(teams[leader][i], 1);
-			next_ede = getCost(teams[leader][i], 0);
-			if (next_ide < next_ede) {
-				result += getCost(teams[leader][i],1);
-				found = 1;
-			}
-			else if (!found && next_ede <= next_ide) {
-				if (next_ide-next_ede < min_cost) {
-					min_cost = next_ide-next_ede;
-					min_node = teams[leader][i];
-				}
-			}
-		}
-		if (!found&& min_node != -1) {
-			for (int i = 0; i < teams[leader].size();i++) {
-				if (teams[leader][i] == min_node) {
-					result += getCost(teams[leader][i], 1);
-				}
-				else {
-					result += getCost(teams[leader][i], 0);
-				}
-			}
-		}	
-	}
-
-
-	cost[leader][is_attend] = result;
-	
-	//cout << "cost["<<leader<<"]["<<is_attend<<"] result : " << result << "\n";
-	return result;
 }
