d505e875fa3dd41af3861e2fd597d34e96392ee4
hyeweon <berry_na@khu.ac.kr>
2021-08-15 23:20:34 +0900

    Update hyeweon.cpp
    week7

--- 7-week7/1/hyeweon.cpp
+++ 7-week7/1/hyeweon.cpp
@@ -10,8 +10,9 @@ int main()
 	for (int i = 0; i < n; i++)
 		cin >> blocks[i];
 
-	double sum = (double)blocks[n - 1];
+	double sum = blocks[n - 1];
 	int answer = 1;
+	int count = 1;
 	for (int i = n - 2; i >= 0; i--) {
 		if ((blocks[i] - m > blocks[i + 1]) || (blocks[i] + m < blocks[i + 1])) {
 			answer = 0;
@@ -21,7 +22,8 @@ int main()
 			answer = 0;
 			break;
 		}
-		sum = (sum * (n - i - 1) + blocks[i]) / double(n - i);
+		sum = (sum * count + blocks[i]) / (count + 1);
+		count++;
 	}
 
 	cout << answer << endl;
