a504924afddf2bf8878aed69df80b98474ef6079
KMileStone <tdj03204@naver.com>
2021-08-15 18:07:59 +0900

    submit 7-week7/2

--- 7-week7/2/KMileStone.py
+++ 7-week7/2/KMileStone.py
@@ -1,23 +1,48 @@
 str = list(input())
 
 
-password = []
-cursor = 0
+# double linked list
+class Node:
+    def __init__(self, char=''):
+        self.char = char
+        self.next = None
+        self.prev = None
+
+    def add(self, char):
+        node = Node(char)
+        node.prev = self
+        node.next = self.next
+        if self.next:
+            self.next.prev = node
+        self.next = node
+
+    def delete(self):
+        self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+        del self
+
+head = Node('head')
+cursor = head
 
 for c in str:
     if c == '<':
-        if cursor > 0:
-            cursor -= 1
+        if cursor.char != 'head':
+            cursor = cursor.prev
     elif c == '>':
-        if cursor < len(password):
-            cursor += 1
+        if cursor.next:
+            cursor = cursor.next
     elif c == '-':
-        if cursor > 0:
-            del password[cursor-1]
-            cursor -= 1
+        if cursor.char != 'head':
+            temp = cursor
+            cursor = cursor.prev
+            temp.delete()
     else:
-        password.insert(cursor, c)
-        cursor += 1
+        cursor.add(c)
+        cursor = cursor.next
 
 
-print(''.join(password))
+cursor = head.next
+while cursor:
+    print(cursor.char, end='')
+    cursor = cursor.next
