a06443b098fc3a58312848068eda455d209301e8
bluejoyq <bluejoyq@naver.com>
2021-08-16 14:50:46 +0900

    8-2 solve

---
+++ 8-week8/2/bluejoyq.py
@@ -0,0 +1,144 @@
+import sys
+sys.setrecursionlimit(100000)
+MAX = sys.maxsize
+def solve(datas = None):
+    N = 10
+    if datas == None:
+        paper = [list(map(int, input().split())) for i in range(N)]
+    else:
+        paper = [list(map(int,tmp.split())) for tmp in datas.split("\n")]
+    used = [0] * 5
+    visited = [[0] * N for i in range(N)]
+    def check(r,c,i):
+        try:
+            for plus in range(i):
+                if not paper[r + plus][c + i] or not paper[r + i][c + plus]:
+                    return 0
+            
+            if not paper[r + i ][c + i]:
+                return 0
+        except:
+            return 0
+        return 1  
+    def toggle(r,c,i):
+        '''
+        for plus in range(i):
+            visited[r + plus][c + i] = 1 - visited[r + plus][c + i]
+            visited[r + i][c + plus] = 1 - visited[r + i][c + plus]
+        
+        visited[r + i ][c + i] = 1 - visited[r + i ][c + i]
+        '''
+        for rr in range(r, r + i + 1):
+            for cc in range(c, c + i + 1):
+                visited[rr][cc] = 1 - visited[rr][cc]
+    def find_best():
+
+        result = MAX
+        for r in range(N):
+            for c in range(N):
+                if not paper[r][c] or visited[r][c]:
+                    continue
+                
+                for i in range(5):
+                    if not check(r,c,i):
+                        break
+                    if used[i] == 5:
+                        continue
+                    
+                    toggle(r,c,i)
+                    used[i] += 1
+
+                    result = min(result ,find_best())
+
+                    toggle(r,c,i)
+                    used[i] -= 1
+
+                return result
+
+        return sum(used)
+    tmp = find_best()
+    if tmp == MAX:
+        return -1
+    return tmp
+print(solve())
+
+assert(solve('''0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0''') == 0)
+assert(solve('''0 0 0 0 0 0 0 0 0 0
+0 1 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 1 0 0 0 0 0
+0 0 0 0 0 1 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0''') == 4)
+assert(solve('''0 0 0 0 0 0 0 0 0 0
+0 1 1 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 0 1 1 0 0 0 0
+0 0 0 0 1 1 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0''') == 5)
+assert(solve('''0 0 0 0 0 0 0 0 0 0
+0 1 1 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 0 1 1 0 0 0 0
+0 0 0 0 0 1 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0''') == -1)
+assert(solve('''0 0 0 0 0 0 0 0 0 0
+0 1 1 0 0 0 0 0 0 0
+0 1 1 1 0 0 0 0 0 0
+0 0 1 1 1 1 1 0 0 0
+0 0 0 1 1 1 1 0 0 0
+0 0 0 0 1 1 1 0 0 0
+0 0 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0''') == 7)
+assert(solve('''1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1 1''') == 4)
+assert(solve('''0 0 0 0 0 0 0 0 0 0
+0 1 1 1 1 1 0 0 0 0
+0 1 1 1 1 1 0 0 0 0
+0 0 1 1 1 1 0 0 0 0
+0 0 1 1 1 1 0 0 0 0
+0 1 1 1 1 1 1 1 1 1
+0 1 1 1 0 1 1 1 1 1
+0 1 1 1 0 1 1 1 1 1
+0 0 0 0 0 1 1 1 1 1
+0 0 0 0 0 1 1 1 1 1''') == 6)
+assert(solve('''0 0 0 0 0 0 0 0 0 0
+1 1 1 1 1 0 0 0 0 0
+1 1 1 1 1 0 1 1 1 1
+1 1 1 1 1 0 1 1 1 1
+1 1 1 1 1 0 1 1 1 1
+1 1 1 1 1 0 1 1 1 1
+0 0 0 0 0 0 0 0 0 0
+0 1 1 1 0 1 1 0 0 0
+0 1 1 1 0 1 1 0 0 0
+0 1 1 1 0 0 0 0 0 1''') == 5)
