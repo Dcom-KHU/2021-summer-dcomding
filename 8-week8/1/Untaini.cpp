76d5a746aa1a88cc9e05284f5fe8ae6e5a4c580a
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-08-16 08:20:11 +0000

    Upload 8-week8/1 solving code

---
+++ 8-week8/1/Untaini.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstring>
+#define abs(a) (a>0?a:-a)
+
+char str[100001];
+int len, half, res;
+
+int main() {
+	scanf("%s",str);
+	len = strlen(str)-1, half = len>>1;
+	
+	for(int cnt=0;cnt<=half;++cnt){
+		if(str[cnt+(res>0)] != str[len-cnt-(res<0)])
+			if(res){
+				res=2;
+				break;
+			}
+			else{
+				res = (str[cnt+1]==str[len-cnt]) - (str[cnt]==str[len-cnt-1]);
+				if(!res) {
+					res=2;
+					break;
+				}
+			}
+	}
+	
+	printf("%d",abs(res));
+	
+	return 0;
+}
