5e6e9450f5f5d8b43974e284f8eb5059c6cdf120
‍손현수(소프트웨어융합대학 컴퓨터공학과) <hgsu0806@khu.ac.kr>
2021-08-16 08:20:26 +0000

    Upload 8-week8/2 solving code

---
+++ 8-week8/2/Untaini.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+
+int paperCnt[6], paper[10][10], usedPaper[10][10], res;
+bool allCanPost=true;
+int main() {
+	for(int nCnt=0;nCnt<10;++nCnt)
+		for(int mCnt=0;mCnt<10;++mCnt)
+			scanf("%d",&paper[nCnt][mCnt]);
+	
+	
+	for(int paperSize=5; paperSize>0; --paperSize){
+		for(int nCnt=0;nCnt<10;++nCnt)
+			for(int mCnt=0;mCnt<10;++mCnt)
+				if(paper[nCnt][mCnt]){
+					if(nCnt+paperSize>10 || mCnt+paperSize>10)
+						continue;
+					
+					bool canPost = true;
+					for(int iCnt=nCnt; iCnt<nCnt+paperSize; ++iCnt)
+						for(int jCnt=mCnt; jCnt<mCnt+paperSize; ++jCnt)
+							canPost &= paper[iCnt][jCnt] && !usedPaper[iCnt][jCnt];
+					
+					if(canPost){
+						++res, ++paperCnt[paperSize];
+						for(int iCnt=nCnt; iCnt<nCnt+paperSize; ++iCnt)
+							for(int jCnt=mCnt; jCnt<mCnt+paperSize; ++jCnt)
+								++usedPaper[iCnt][jCnt];
+					}
+				}
+		
+		allCanPost = paperCnt[paperSize]<=5;
+	}
+	printf("%d",allCanPost?res:-1);
+	return 0;
+}
