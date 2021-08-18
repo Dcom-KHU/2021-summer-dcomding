451c5d6804146d743d903b370b643b61944ec15f
BuildTools <unconfigured@null.spigotmc.org>
2021-08-19 05:12:23 +0900

    8-2 solve?

--- 8-week8/2/bluejoyq.py
+++ 8-week8/2/bluejoyq.py
@@ -1,14 +1,21 @@
 import sys
 sys.setrecursionlimit(100000)
 MAX = sys.maxsize
+
+
+
 def solve(datas = None):
+    global best_result
     N = 10
     if datas == None:
         paper = [list(map(int, input().split())) for i in range(N)]
     else:
         paper = [list(map(int,tmp.split())) for tmp in datas.split("\n")]
+    
     used = [0] * 5
     visited = [[0] * N for i in range(N)]
+    best_result = MAX
+
     def check(r,c,i):
         
         try:
@@ -21,16 +28,7 @@ def solve(datas = None):
         except:
             return 0
         return 1  
-        '''
-        try:
-            for rr in range(r, r + i + 1):
-                for cc in range(c, c + i + 1):
-                    if not paper[rr][cc]:
-                        return 0
-            return 1
-        except:
-            return 0
-        '''
+
     def check_range(r,c):
         for i in range(4):
             if check(r,c,i):
@@ -43,49 +41,10 @@ def solve(datas = None):
             visited[r + i][c + plus] = 1 - visited[r + i][c + plus]
         
         visited[r + i ][c + i] = 1 - visited[r + i ][c + i]
-        '''
-        for rr in range(r, r + i + 1):
-            for cc in range(c, c + i + 1):
-                visited[rr][cc] = 1 - visited[rr][cc]
-        '''
 
-    def do_something(r,c):
-        
-        result = MAX
-        for i in range(4):
-            if not check(r,c,i):
-                i -= 1
-                break
 
-        for tmp in range(i, -1, -1):
-            toggle(r,c,tmp)
-            
-        for tmp in range(i, -1, -1):
-            if used[tmp] == 5:
-                continue
-
-            used[tmp] += 1
-            result = min(result ,find_best(r,c  + 1))
-            used[tmp] -= 1
-            toggle(r,c,tmp)
-            
-        return result
-    def find_best(start_r, start_c):
-        
-        
-        r= start_r
-        
-        for c in range(start_c):
-            if not paper[r][c] or visited[r][c]:
-                continue
-            return do_something(r,c)
-        for r in range(N):
-            for c in range(N):
-                if not paper[r][c] or visited[r][c]:
-                    continue
-                return do_something(r,c)
-        return sum(used)
-
+    
+    # bfs로 해야하나? -> 아냐 그러면 최소 100 * 100이 수두룩
     # dfs로 가지치기를 해야함. 더크면 바로 중단하게 
     # 쭉 돌면서 1인 곳을 메모부터 할까? 
     # 그리고 가능한 range도 ㅋㅋ
@@ -95,18 +54,42 @@ def solve(datas = None):
         for c in range(N):
             if not paper[r][c]:
                 continue
-            idx = check_range(r,c)
-            selects.append(([r,c], idx))
-    
-    for select in selects:
+            possible = check_range(r,c)
+            # 위치, 가능한 사각형 범위 0~4
+            selects.append(([r,c], possible))
+
+    goal = len(selects)
+    # 인자 하나 더주고 가지치기 하는게 더 나음
+    def recur_find_best(idx):
+        global best_result
+        if idx == goal:
+            best_result = min(best_result, sum(used))
+            return 
+        elif sum(used) > best_result:
+            return
+
+        [r,c], possible = selects[idx]
+
+        if visited[r][c]:
+            recur_find_best(idx + 1)
+        
+        for tmp in range(possible):
+            toggle(r,c,tmp)
+        for p in range(possible, -1, -1):
+            if used[p] == 4:
+                continue
+            recur_find_best(idx)
+            toggle(r,c,p)
+
+    recur_find_best(0)
+        
 
 
-
-    print("##############",real_result)
-    if real_result == MAX:
+    #print("##############",best_result)
+    if best_result == MAX:
         return -1
-    return real_result
-#print(solve())
+    return best_result
+print(solve())
 
 assert(solve('''0 0 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0 0 0
