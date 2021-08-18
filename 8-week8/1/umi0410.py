604c9f617f2fbd65c19cad92abb7d9cf012eccdc
Jinsu Park <dev.umijs@gmail.com>
2021-08-18 20:07:46 +0900

    js

--- 8-week8/1/umi0410.py
+++ 8-week8/1/umi0410.py
@@ -1,28 +1,17 @@
-s = input()
-has_diff = False
-for i in range(len(s)//2):
-    if s[i] != s[-i-1]:
-        has_diff = True
-        break
-if not has_diff:
-    print(0)
-else:
-    # 하나라도 전체가 같은 경우가 있는지 체크
-    for i in range(len(s)):
-        _s = s[:i] + s[i+1:]
-        has_diff = False
-        # 하나라도 다른지 체크
-        for j in range(len(_s)//2):
-            # print(_s[j], _s[-j - 1])
-            if _s[j] != _s[-j - 1]:
-                has_diff = True
-                break
-        # 모두 같은 애가 하나라도 있는지 체크
-        if not has_diff:
-            # print(_s)
-            break
-    if not has_diff:
-        print(1)
+import sys
+input = sys.stdin.readline
+
+def check(s, diff_cnt):
+    if diff_cnt == 2:
+        return 2
     else:
-        print(2)
+        for i in range(len(s)//2):
+            if s[i] != s[len(s)-i-1]:
+                return min(check(s[:i] + s[i+1:], diff_cnt + 1),
+                           check(s[:len(s)-i-1] + s[len(s)-i:], diff_cnt + 1))
+        return diff_cnt
+
+for _ in range(int(input())):
+    s = input().strip()
+    print(check(s, 0))
 
