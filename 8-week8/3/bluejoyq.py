7788602013b287f4d08091413961430cb895fb85
bluejoyq <bluejoyq@naver.com>
2021-08-20 02:56:02 +0900

    8-3 change visited size as not using bit_idx

--- 8-week8/3/bluejoyq.py
+++ 8-week8/3/bluejoyq.py
@@ -1,7 +1,22 @@
 from heapq import heappop, heappush
 import sys
+# import time
+# import random
+
+# def decorator(func):   
+    
+#     def wrapper(*args, **kwargs):   
+#         bef = time.time() 
+#         result = func(*args, **kwargs)        
+#         aft = time.time()
+#         print(aft - bef)
+#         return result
+    
+#     return wrapper               
+
 input = sys.stdin.readline
 MAX = sys.maxsize
+
 def solution():
     def sperate_input():
         return map(int,input().split())
@@ -15,15 +30,46 @@ def solution():
     for i in range(m):
         p,q,t = sperate_input()
         roads[p][q] = min(t, roads[p][q] )
-
     
+    visited = [0] * (n + 1) 
+    # n = 1000
+    # m = 3000
+    # k = 10
+    # start, end = 1, 1000
+    # roads = [[MAX] * (n + 1) for i in range(n + 1)]
+    # traps = [random.randint(2,999) for i in range(k)]
+    # trap_checker = [-1] * (n + 1)
+    # for i in range(k):
+    #     trap_checker[traps[i]] = i
 
+    # for i in range(m):
+    #     p,q,t = random.randint(1,1000), random.randint(1,1000), random.randint(1,3000)
+    #     roads[p][q] = min(t, roads[p][q] )
+    # roads[1][1000] = 3000 * 3000
     def exchange(target, idx):
         # 함정방을 위한 교환
         tmp = target[idx][:]
         for i in range(n + 1):
             target[idx][i] = target[i][idx]
             target[i][idx] = tmp[i]
+    
+    def is_visited(cur_pos, bit_idx):
+        if trap_checker[cur_pos] != -1:
+            activated= int(bool( bit_idx  & (1 << trap_checker[cur_pos])))
+            if visited[cur_pos][activated]:
+                return 1
+        else:
+            if visited[cur_pos]:
+                return 1
+        return 0
+
+    def visit(cur_pos, bit_idx):
+        if trap_checker[cur_pos] != -1:
+            activated=int(bool( bit_idx  & (1 << trap_checker[cur_pos])))
+            visited[cur_pos][activated] = 1
+        else:
+            visited[cur_pos] = 1
+
     # 함정방의 최대 개수가 10개이므로 비트마스크를 써야함.
     # road를 각 함정방의 경우에 수의 따라 다 만들어 놓자.
     # 최대가 3000 * 1000 = 3백만
@@ -39,19 +85,21 @@ def solution():
                 continue
             idx = traps[j]
             exchange(roads_by_trap[i], idx)
-    visited = [[0] * (n + 1) for i in range(bit_max)]
+    
+    for trap in traps:
+        visited[trap] = [0,0]
     findings = []
     # cost, cur_pos, bit_idx
     heappush(findings, (0, start, 0))
 
     while findings:
         cur_cost, cur_pos, bit_idx = heappop(findings)
-        if visited[bit_idx][cur_pos]:
+        if is_visited(cur_pos, bit_idx):
             continue
         # 종결 조건
         if cur_pos == end:
             return cur_cost
-        visited[bit_idx][cur_pos] = 1
+        visit(cur_pos, bit_idx)
         cur_road = roads_by_trap[bit_idx][cur_pos]
         for nxt_pos in range(1, n + 1):
             cost = cur_road[nxt_pos]
@@ -60,11 +108,11 @@ def solution():
             
             if trap_checker[nxt_pos] != -1:
                 new_bit_idx = bit_idx ^ (1 << trap_checker[nxt_pos])
-                if visited[new_bit_idx][nxt_pos]:
+                if is_visited(nxt_pos, new_bit_idx):
                     continue
                 heappush(findings, (cur_cost + cost, nxt_pos, new_bit_idx))
             else:
-                if visited[bit_idx][nxt_pos]:
+                if is_visited(nxt_pos, bit_idx):
                     continue
                 heappush(findings, (cur_cost + cost, nxt_pos, bit_idx))
     
