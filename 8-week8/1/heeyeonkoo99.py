22707cef3ea0194d14f70d707bad22e9e5d9159e
Heeyeon Koo <rndus0819@naver.com>
2021-08-21 20:50:07 +0900

    week8-1(revised)

--- 8-week8/1/heeyeonkoo99.py
+++ 8-week8/1/heeyeonkoo99.py
@@ -1,4 +1,4 @@
-def check(left,right):
+def check(s,left,right):
     while left<right:
         if s[left]==s[right]:
             left+=1
@@ -7,19 +7,17 @@ def check(left,right):
             return False
     return True
 
-def twopointer(left,right):
+def twopointer(s,left,right):
     while left<right:
         if s[left]==s[right]:
             left+=1
             right-=1
         else:
-            if check(left+1,right) or check(left,right-1): #그 전후 원소들은 볼필요가 없다 이미 같으니까!
+            if check(s,left+1,right) or check(s,left,right-1): #그 전후 원소들은 볼필요가 없다 이미 같으니까!
                 return 1
             return 2
-        return 0
+    return 0
 T = int(input()) 
 for _ in range(T): 
     s = input() 
-    print(twopointer(0, len(s)-1))
-
-#Review) 투포인터 알고리즘 사용하기!
+    print(twopointer(s,0, len(s)-1))
