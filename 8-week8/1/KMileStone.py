47bbe3f86cd3af15228a277023fee487fb945d4a
KMileStone <tdj03204@naver.com>
2021-08-22 19:41:12 +0900

    submit 8-week8/1

---
+++ 8-week8/1/KMileStone.py
@@ -0,0 +1,29 @@
+word = input()
+
+
+count = 0
+left = 0
+right = len(word)-1
+
+while left < right:
+    if word[left] == word[right]:
+        left += 1
+        right -= 1
+    elif count < 2:
+        count += 1
+
+        # if left letter is obstacle
+        if left+1 < right and word[left+1] == word[right]:
+            left += 1
+
+        # if right letter is obstacle
+        elif left < right-1 and word[left] == word[right-1]:
+            right -= 1
+
+        # if different at all
+        else:
+            count = 2
+            break
+
+
+print(count)
