f4b19d4e3456c351f42bd0390e245a0bc9f03b41
bluejoyq <bluejoyq@naver.com>
2021-08-22 15:47:49 +0900

    8-3 real fast

--- 8-week8/3/bluejoyq.py
+++ 8-week8/3/bluejoyq.py
@@ -11,11 +11,16 @@ def solution():
     for i in range(k):
         trap_checker[traps[i]] = i
 
-    roads = [[MAX] * (n + 1) for i in range(n + 1)]
+    roads = {i:{} for i in range(1,n + 1) }
+    reversed_roads = {i:{} for i in range(1,n + 1) }
     for i in range(m):
         p,q,t = map(int,input().split())
-        roads[p][q] = min(t, roads[p][q] )
-    
+        try:
+            roads[p][q] = min(t, roads[p][q] )
+            reversed_roads[q][p] = min(t, reversed_roads[q][p] )
+        except:
+            roads[p][q] = t
+            reversed_roads[q][p] = t
     # 함정방의 최대 개수가 10개이므로 비트마스크를 써야함.
     # road를 각 함정방의 경우에 수의 따라 다 만들어 놓자. -> 시간 초과의 원인이 이거네
     # 최대가 3000 * 1000 = 3백만
@@ -57,27 +62,33 @@ def solution():
         # 이번이 거꾸로 되있다면.
         if trap_checker[cur_pos] != -1 and bit_idx & bit_check[trap_checker[cur_pos]]:
             # 역방향 간선들에 대해
-            for nxt_pos in range(1, n + 1):
-                cost = roads[nxt_pos][cur_pos]
-                if cost == MAX or is_reversed(bit_idx,nxt_pos):
+            for nxt_pos in reversed_roads[cur_pos].keys():
+                cost = reversed_roads[cur_pos][nxt_pos]
+                if is_reversed(bit_idx,nxt_pos):
                     continue
                 visit(bit_idx, cur_cost,nxt_pos, cost)
             
             # 역방항에서 정방향 간선으로 갈 수 있는 경우는 얘네가 함정인 경우 뿐
 
             for nxt_pos in traps:
-                cost = roads[cur_pos][nxt_pos]
-                if cost != MAX and bit_idx & bit_check[trap_checker[nxt_pos]]:
-                    visit(bit_idx, cur_cost,nxt_pos, cost)
+                try:
+                    cost = roads[cur_pos][nxt_pos]
+                    if bit_idx & bit_check[trap_checker[nxt_pos]]:
+                        visit(bit_idx, cur_cost,nxt_pos, cost)
+                except:
+                    continue
         else: # 여기가 정방향이면
-            for nxt_pos in range(1, n + 1):
+            for nxt_pos in roads[cur_pos].keys():
                 cost = roads[cur_pos][nxt_pos]
-                if cost == MAX or is_reversed(bit_idx,nxt_pos):
+                if is_reversed(bit_idx,nxt_pos):
                     continue
                 visit(bit_idx, cur_cost,nxt_pos, cost)
             for nxt_pos in traps:
-                cost = roads[nxt_pos][cur_pos]
-                if cost != MAX and bit_idx & bit_check[trap_checker[nxt_pos]]:
-                    visit(bit_idx, cur_cost,nxt_pos, cost)
+                try:
+                    cost = reversed_roads[cur_pos][nxt_pos]
+                    if bit_idx & bit_check[trap_checker[nxt_pos]]:
+                        visit(bit_idx, cur_cost,nxt_pos, cost)
+                except:
+                    continue
     return -1
 print(solution())
