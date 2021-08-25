f77d3f21bc04553d5af4b4b6d7e19c529d9fb78f
BuildTools <unconfigured@null.spigotmc.org>
2021-08-26 02:30:18 +0900

    7-5 resolve

--- 8-week8/3/bluejoyq.py
+++ 8-week8/3/bluejoyq.py
@@ -1,51 +1,93 @@
-import heapq
-import sys
-input = sys.stdin.readline
-
 def solution():
+    from heapq import heappop, heappush
+    import sys
+
+    input = sys.stdin.readline
+    MAX = sys.maxsize
     n,m,k,start,end = map(int,input().split())
-    edge = [[] for _ in range(n + 1)]
-    traps = tuple(map(int,input().split()))
-    mask_idx = {t : n for n, t in enumerate(traps)}
-    
+    traps = list(map(int,input().split()))
+    trap_checker = [-1] * (n + 1)
+    for i in range(k):
+        trap_checker[traps[i]] = i
 
+    roads = {i:{} for i in range(1,n + 1) }
+    reversed_roads = {i:{} for i in range(1,n + 1) }
     for i in range(m):
-        a, b, d = map(int,input().split())
-        edge[a].append((b, d))
-        edge[b].append((a, -d))
+        p,q,t = map(int,input().split())
+        try:
+            roads[p][q] = min(t, roads[p][q] )
+            reversed_roads[q][p] = min(t, reversed_roads[q][p] )
+        except:
+            roads[p][q] = t
+            reversed_roads[q][p] = t
+    # 함정방의 최대 개수가 10개이므로 비트마스크를 써야함.
+    # road를 각 함정방의 경우에 수의 따라 다 만들어 놓자. -> 시간 초과의 원인이 이거네
+    # 최대가 3000 * 1000 = 3백만
+    bit_max = pow(2,k)
+    bit_check = [pow(2, i) for i in range(k)]
 
-    # bfs
-    heap = [(0, start, 0)]
-    dist = {}
-    answer = -1
-    while heap:
-        d, idx, mask = heapq.heappop(heap)
-        if dist.get((idx, mask), None):
+    visited = [[0] * (n + 1) for i in range(bit_max)]
+    findings = []
+
+    # cost, cur_pos, bit_idx
+    heappush(findings, (0, start, 0))
+
+    def visit(bit_idx, cur_cost,nxt_pos, cost):
+        # 다음 길이 트랩이라면
+        if trap_checker[nxt_pos] != -1:
+            new_bit_idx = bit_idx ^ bit_check[trap_checker[nxt_pos]]
+            if visited[new_bit_idx][nxt_pos]:
+                return
+            heappush(findings, (cur_cost + cost, nxt_pos, new_bit_idx))
+        else:
+            if visited[bit_idx][nxt_pos]:
+                return
+            heappush(findings, (cur_cost + cost, nxt_pos, bit_idx))
+    
+    def is_reversed(bit_idx,pos):
+        if trap_checker[pos] != -1 and bit_idx & bit_check[trap_checker[pos]]:
+            return True
+        return False
+    while findings:
+
+        cur_cost, cur_pos, bit_idx = heappop(findings)
+        if visited[bit_idx][cur_pos]:
             continue
+        # 종결 조건
+        if cur_pos == end:
+            return cur_cost
+        visited[bit_idx][cur_pos] = 1
 
-        dist[(idx, mask)] = d
-        if idx == end:
-            answer = d
-            break;
+        # 이번이 거꾸로 되있다면.
+        if trap_checker[cur_pos] != -1 and bit_idx & bit_check[trap_checker[cur_pos]]:
+            # 역방향 간선들에 대해
+            for nxt_pos in reversed_roads[cur_pos].keys():
+                cost = reversed_roads[cur_pos][nxt_pos]
+                if is_reversed(bit_idx,nxt_pos):
+                    continue
+                visit(bit_idx, cur_cost,nxt_pos, cost)
+            
+            # 역방항에서 정방향 간선으로 갈 수 있는 경우는 얘네가 함정인 경우 뿐
 
-        direction = 1
-        if idx in traps and (mask & (1 << mask_idx[idx])):
-            direction *= -1
-
-        for near_idx, near_d in edge[idx]:
-            if near_idx in traps and (mask & (1 << mask_idx[near_idx])):
-                near_d *= -1
-
-            if near_d * direction > 0:
-                new_mask = mask
-                if near_idx in traps:
-                    if mask & (1 << mask_idx[near_idx]):
-                        new_mask = mask & ~(1 << mask_idx[near_idx])
-                    else:
-                        new_mask = mask | (1 << mask_idx[near_idx]) 
-
-                heapq.heappush(heap, (d + near_d * direction, near_idx, new_mask))
-
-
-    return answer
+            for nxt_pos in traps:
+                try:
+                    cost = roads[cur_pos][nxt_pos]
+                    if bit_idx & bit_check[trap_checker[nxt_pos]]:
+                        visit(bit_idx, cur_cost,nxt_pos, cost)
+                except:
+                    continue
+        else: # 여기가 정방향이면
+            for nxt_pos in roads[cur_pos].keys():
+                cost = roads[cur_pos][nxt_pos]
+                if is_reversed(bit_idx,nxt_pos):
+                    continue
+                visit(bit_idx, cur_cost,nxt_pos, cost)
+            for nxt_pos in traps:
+                try:
+                    cost = reversed_roads[cur_pos][nxt_pos]
+                    if bit_idx & bit_check[trap_checker[nxt_pos]]:
+                        visit(bit_idx, cur_cost,nxt_pos, cost)
+                except:
+                    continue
+    return -1
 print(solution())
